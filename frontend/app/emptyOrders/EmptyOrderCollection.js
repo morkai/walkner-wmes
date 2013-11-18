3f9a9907770d69aacd20f65633f300866a9780b0
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2013-11-18 23:27:32 +0100

    Changed the default empty order list filter to include empty orders only from today.

--- frontend/app/emptyOrders/EmptyOrderCollection.js
+++ frontend/app/emptyOrders/EmptyOrderCollection.js
@@ -11,8 +11,24 @@ define([
 
     model: EmptyOrder,
 
-    rqlQuery:
-      'select(nc12,mrp,startDate,finishDate,statuses)&sort(-startDate,-finishDate,nc12)&limit(15)'
+    rqlQuery: function(rql)
+    {
+      var today = new Date();
+      today.setHours(0);
+      today.setMinutes(0);
+      today.setSeconds(0);
+      today.setMilliseconds(0);
+
+      return rql.Query.fromObject({
+        fields: {nc12: 1, mrp: 1, startDate: 1, finishDate: 1},
+        sort: {startDate: -1, finishDate: -1},
+        limit: 15,
+        selector: {
+          name: 'and',
+          args: [{name: 'eq', args: ['startDate', today.getTime()]}]
+        }
+      });
+    }
 
   });
 });
