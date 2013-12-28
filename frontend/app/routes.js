459dd6b58bb15a0fa4a71dcff3d74979b5ab7fc5
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2013-12-28 13:42:23 +0100

    Moved routes modules dependencies to a separate file.

---
+++ frontend/app/routes.js
@@ -0,0 +1,29 @@
+define([
+  './aors/routes',
+  './companies/routes',
+  './dashboard/routes',
+  './divisions/routes',
+  './downtimeReasons/routes',
+  './events/routes',
+  './emptyOrders/routes',
+  './fte/routes',
+  './hourlyPlans/routes',
+  './mechOrders/routes',
+  './mrpControllers/routes',
+  './orders/routes',
+  './orderStatuses/routes',
+  './prodDowntimes/routes',
+  './prodFlows/routes',
+  './prodLines/routes',
+  './prodTasks/routes',
+  './production/routes',
+  './subdivisions/routes',
+  './users/routes',
+  './vis/routes',
+  './workCenters/routes'
+], function()
+{
+  'use strict';
+
+
+});
