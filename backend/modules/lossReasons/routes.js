8150eb6aa748655da07d0d311cc83fcd95a772d7
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-01-06 20:41:19 +0100

    Added a missing `lossReasons` backend module.

---
+++ backend/modules/lossReasons/routes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var crud = require('../express/crud');
+
+module.exports = function setUpLossReasonsRoutes(app, lossReasonsModule)
+{
+  var express = app[lossReasonsModule.config.expressId];
+  var auth = app[lossReasonsModule.config.userId].auth;
+  var LossReason = app[lossReasonsModule.config.mongooseId].model('LossReason');
+
+  var canView = auth('DICTIONARIES:VIEW');
+  var canManage = auth('DICTIONARIES:MANAGE');
+
+  express.get('/lossReasons', canView, crud.browseRoute.bind(null, app, LossReason));
+
+  express.post('/lossReasons', canManage, crud.addRoute.bind(null, app, LossReason));
+
+  express.get('/lossReasons/:id', canView, crud.readRoute.bind(null, app, LossReason));
+
+  express.put('/lossReasons/:id', canManage, crud.editRoute.bind(null, app, LossReason));
+
+  express.del('/lossReasons/:id', canManage, crud.deleteRoute.bind(null, app, LossReason));
+};
