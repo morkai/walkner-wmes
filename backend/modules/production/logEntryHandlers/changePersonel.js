00624cad04865b0b2ebd90bebf5d30281d16180e
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-01-30 03:15:00 +0100

    Fill the user info if only the user's personell ID is present (the prod log entry was created offline).

--- backend/modules/production/logEntryHandlers/changePersonel.js
+++ backend/modules/production/logEntryHandlers/changePersonel.js
@@ -23,6 +23,63 @@ module.exports = function(personelProperty)
         return done(null);
       }
 
+      if (logEntry.data && logEntry.data.id === null)
+      {
+        fillUserData(prodShift);
+      }
+      else
+      {
+        updateProdShift(prodShift);
+      }
+    });
+
+    function fillUserData(prodShift)
+    {
+      var personellId = logEntry.data.label;
+
+      app[productionModule.config.mongooseId].model('User')
+        .findOne({personellId: personellId}, {_id: 1, firstName: 1, lastName: 1})
+        .lean()
+        .exec(function(err, user)
+        {
+          if (err)
+          {
+            productionModule.error(
+              "Failed to find the user [%s] to fill the %s info (prodLogEntry=[%s]): %s",
+              personellId,
+              personelProperty,
+              logEntry._id,
+              err.stack
+            );
+
+            return done(err);
+          }
+
+          if (!user)
+          {
+            productionModule.warn(
+              "Couldn't find a %s by personellId=[%s] (prodLogEntry=[%s]) :(",
+              personelProperty,
+              personellId,
+              logEntry._id
+            );
+
+            return updateProdShift(prodShift);
+          }
+
+          logEntry.data.id = user._id.toString();
+
+          if (user.firstName && user.lastName)
+          {
+            logEntry.data.label = user.firstName + ' ' + user.lastName;
+          }
+
+          return updateProdShift(prodShift);
+        });
+    }
+
+    function updateProdShift(prodShift)
+    {
       prodShift.set(personelProperty, logEntry.data);
 
       prodShift.save(function(err)
@@ -39,6 +96,6 @@ module.exports = function(personelProperty)
 
         return done(err);
       });
-    });
+    }
   };
 };
