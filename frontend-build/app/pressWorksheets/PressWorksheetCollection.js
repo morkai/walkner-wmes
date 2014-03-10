65ade241717b992d3255f59232fcb5baabd7dd55
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-03-10 16:46:10 +0100

    Updated the frontend build and increased the frontend and backend versions.

--- frontend-build/app/pressWorksheets/PressWorksheetCollection.js
+++ frontend-build/app/pressWorksheets/PressWorksheetCollection.js
@@ -1,1 +1,1 @@
-define(["../core/Collection","./PressWorksheet"],function(e,r){return e.extend({model:r,rqlQuery:"exclude(orders,operators)&sort(-date,-shift)&limit(15)"})});
+define(["../core/Collection","./PressWorksheet"],function(e,r){return e.extend({model:r,rqlQuery:"exclude(orders,operators)&sort(-date)&limit(15)"})});
