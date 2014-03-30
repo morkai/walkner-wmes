ebe2af0d7bb20eff9aceed19de1b38ca7aeab487
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-03-30 22:52:20 +0200

    Refactored the `changeShift` log entry handler to use h5.step.

--- backend/modules/production/logEntryHandlers/changeShift.js
+++ backend/modules/production/logEntryHandlers/changeShift.js
@@ -9,143 +9,70 @@ module.exports = function(app, productionModule, prodLine, logEntry, done)
   var ProdShiftOrder = mongoose.model('ProdShiftOrder');
   var ProdDowntime = mongoose.model('ProdDowntime');
 
-  finishOrders();
-
-  function finishOrders()
-  {
-    ProdShiftOrder
-      .find({prodLine: prodLine._id, finishedAt: null})
-      .sort({date: 1})
-      .exec(function(err, prodShiftOrders)
-      {
-        if (err)
-        {
-          productionModule.error(
-            "Failed to find unfinished prod shift orders for prod line [%s] (LOG=[%s]): %s",
-            prodLine._id,
-            logEntry._id,
-            err.stack
-          );
-
-          return finishDowntimes();
-        }
-
-        if (prodShiftOrders.length === 0)
-        {
-          return finishDowntimes();
-        }
-
-        productionModule.debug(
-          "Finishing %d bugged prod shift orders for prod line [%s] (LOG=[%s])...",
-          prodShiftOrders.length,
-          prodLine._id,
-          logEntry._id
-        );
-
-        step(
-          function finishOrdersStep()
-          {
-            var step = this;
-
-            prodShiftOrders.forEach(function(prodShiftOrder)
-            {
-              finishBugged('prod shift order', prodShiftOrder, step.parallel());
-            });
-          },
-          finishDowntimes
-        );
-      });
-  }
-
-  function finishDowntimes()
-  {
-    ProdDowntime
-      .find({prodLine: prodLine._id, finishedAt: null})
-      .sort({date: 1})
-      .exec(function(err, prodDowntimes)
-      {
-        if (err)
-        {
-          productionModule.error(
-            "Failed to find unfinished prod downtimes for prod line [%s] (LOG=[%s]): %s",
-            prodLine._id,
-            logEntry._id,
-            err.stack
-          );
-
-          return createNewShift();
-        }
-
-        if (prodDowntimes.length === 0)
-        {
-          return createNewShift();
-        }
-
-        productionModule.debug(
-          "Finishing %d bugged prod downtimes for prod line [%s] (LOG=[%s])...",
-          prodDowntimes.length,
-          prodLine._id,
-          logEntry._id
-        );
-
-        step(
-          function finishDowntimesStep()
-          {
-            var step = this;
-
-            prodDowntimes.forEach(function(prodDowntime)
-            {
-              finishBugged('prod downtime', prodDowntime, step.parallel());
-            });
-          },
-          createNewShift
-        );
-      });
-  }
-
-  function finishBugged(type, buggedProdModel, done)
-  {
-    var _id = buggedProdModel._id;
-
-    productionModule.getProdData(null, _id, function(err, cachedProdModel)
+  step(
+    function findOrdersAndDowntimesToFinishStep()
     {
-      if (cachedProdModel)
+      var conditions = {
+        startedAt: {$gt: Date.now() - 7 * 24 * 3600 * 1000},
+        prodLine: prodLine._id,
+        finishedAt: null
+      };
+
+      ProdShiftOrder.find(conditions).exec(this.parallel());
+      ProdDowntime.find(conditions).exec(this.parallel());
+    },
+    function finishOrdersAndDowntimesStep(err, prodShiftOrders, prodDowntimes)
+    {
+      if (err)
       {
-        buggedProdModel = cachedProdModel;
+        return productionModule.error(
+          "Failed to find unfinished orders and downtimes for prod line [%s] (LOG=[%s]): %s",
+          prodLine._id,
+          logEntry._id,
+          err.stack
+        );
       }
 
-      buggedProdModel.finishedAt = new Date(buggedProdModel.date.getTime() + (8 * 3600 * 1000) - 1);
-
-      buggedProdModel.save(function(err)
+      if (prodShiftOrders.length === 0 && prodDowntimes.length === 0)
       {
-        if (err)
-        {
-          productionModule.error(
-            "Failed to save finished %s [%s] for prod line [%s] (LOG=[%s]): %s",
-            type,
-            _id,
-            prodLine._id,
-            logEntry._id,
-            err.stack
-          );
-        }
+        return;
+      }
 
-        return done();
-      });
-    });
-  }
+      productionModule.debug(
+        "Finishing %d bugged prod shift orders and %d bugged prod downtimes for prod line [%s]"
+          + " (LOG=[%s])...",
+        prodShiftOrders.length,
+        prodDowntimes.length,
+        prodLine._id,
+        logEntry._id
+      );
 
-  function createNewShift()
-  {
-    var prodShift = new ProdShift(logEntry.data.startedProdShift);
+      var i;
+      var l;
 
-    prodShift.save(function(err)
+      for (i = 0, l = prodShiftOrders.length; i < l; ++i)
+      {
+        finishBugged('prod shift order', prodShiftOrders[i], this.parallel());
+      }
+
+      for (i = 0, l = prodDowntimes.length; i < l; ++i)
+      {
+        finishBugged('prod downtime', prodDowntimes[i], this.parallel());
+      }
+    },
+    function createProdShiftStep()
+    {
+      var prodShift = new ProdShift(logEntry.data.startedProdShift);
+
+      prodShift.save(this.next());
+    },
+    function handleCreatedProdShiftStep(err, prodShift)
     {
       if (err && err.code !== 11000)
       {
         productionModule.error(
           "Failed to save a new prod shift [%s] for prod line [%s] (LOG=[%s]): %s",
-          prodShift._id,
+          logEntry.prodShift,
           prodLine._id,
           logEntry._id,
           err.stack
@@ -183,6 +110,38 @@ module.exports = function(app, productionModule, prodLine, logEntry, done)
         }
 
         return done(err);
+      });
+    }
+  );
+
+  function finishBugged(type, buggedProdModel, done)
+  {
+    var _id = buggedProdModel._id;
+
+    productionModule.getProdData(null, _id, function(err, cachedProdModel)
+    {
+      if (cachedProdModel)
+      {
+        buggedProdModel = cachedProdModel;
+      }
+
+      buggedProdModel.finishedAt = new Date(buggedProdModel.date.getTime() + (8 * 3600 * 1000) - 1);
+
+      buggedProdModel.save(function(err)
+      {
+        if (err)
+        {
+          productionModule.error(
+            "Failed to save finished %s [%s] for prod line [%s] (LOG=[%s]): %s",
+            type,
+            _id,
+            prodLine._id,
+            logEntry._id,
+            err.stack
+          );
+        }
+
+        return done();
       });
     });
   }
