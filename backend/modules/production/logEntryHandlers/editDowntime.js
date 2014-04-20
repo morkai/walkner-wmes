7bcb510eeaf23f2361cd7a6b32c3d04c18a48623
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-04-21 00:51:08 +0200

    production: editDowntime: recalculate durations of the related ProdShiftOrder after editing the startedAt or finishedAt properties.

--- backend/modules/production/logEntryHandlers/editDowntime.js
+++ backend/modules/production/logEntryHandlers/editDowntime.js
@@ -11,11 +11,51 @@ module.exports = function(app, productionModule, prodLine, logEntry, done)
   var changes = logEntry.data;
 
   step(
-    function getModelsStep()
+    function getProdDowntimeModelStep()
     {
       productionModule.getProdData('downtime', logEntry.data._id, this.next());
     },
-    function updateModelsStep(err, prodDowntime)
+    function getProdShiftOrderModelStep(err, prodDowntime)
+    {
+      if (err)
+      {
+        productionModule.error(
+          "Failed to find downtime [%s] to edit (LOG=[%s]): %s",
+          logEntry.data._id,
+          logEntry._id,
+          err.stack
+        );
+
+        return this.skip(err);
+      }
+
+      var next = this.next();
+      var orderId = prodDowntime.prodShiftOrder && (changes.startedAt || changes.finishedAt);
+
+      if (orderId)
+      {
+        productionModule.getProdData('order', orderId, function(err, prodShiftOrder)
+        {
+          if (err)
+          {
+            productionModule.error(
+              "Failed to find order [%s] while editing downtime [%s] (LOG=[%s]): %s",
+              orderId,
+              logEntry.data._id,
+              logEntry._id,
+              err.stack
+            );
+          }
+
+          next(err, prodDowntime, prodShiftOrder);
+        });
+      }
+      else
+      {
+        next(null, prodDowntime, null);
+      }
+    },
+    function updateProdDowntimeStep(err, prodDowntime, prodShiftOrder)
     {
       if (err)
       {
@@ -32,7 +72,41 @@ module.exports = function(app, productionModule, prodLine, logEntry, done)
       delete changes._id;
 
       prodDowntime.set(changes);
-      prodDowntime.save(this.parallel());
+      prodDowntime.save(this.next());
+
+      this.prodShiftOrder = prodShiftOrder;
+    },
+    function recalcOrderDurationsStep(err)
+    {
+      if (err)
+      {
+        productionModule.error(
+          "Failed to delete downtime [%s] (LOG=[%s]): %s",
+          logEntry.data._id,
+          logEntry._id,
+          err.stack
+        );
+
+        return this.skip(err);
+      }
+
+      if (this.prodShiftOrder)
+      {
+        this.prodShiftOrder.recalcDurations(true, this.next());
+      }
+    },
+    function handleRecalcDurationsResultStep(err)
+    {
+      if (err)
+      {
+        productionModule.error(
+          "Failed to recalc order [%s] durations after editing downtime [%s] (LOG=[%s]): %s",
+          this.prodShiftOrder._id,
+          logEntry.data._id,
+          logEntry._id,
+          err.stack
+        );
+      }
     },
     done
   );
