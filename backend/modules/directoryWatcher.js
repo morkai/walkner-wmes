ffc44d08a3b582521a8d3dc643467242d35d6284
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-07-24 16:23:48 +0200

    directoryWatcher: the `fs.watch()` callback now trigger a delayed, full directory read, because node.js sometimes doesn't trigger, if there was a lot of directory changes in a quick succession.

--- backend/modules/directoryWatcher.js
+++ backend/modules/directoryWatcher.js
@@ -8,39 +8,87 @@ var fs = require('fs');
 var path = require('path');
 
 exports.DEFAULT_CONFIG = {
-  path: './'
+  path: './',
+  delay: 2500,
+  maxDelay: 10000
 };
 
 exports.start = function startDirectoryWatcherModule(app, module)
 {
   var FILE_NAME_RE = /^(?:([0-9]{10,})@)?(.*?)$/;
+  var timer = null;
+  var timerStartedAt = -1;
+  var readingDir = false;
+  var readDirAgain = false;
 
   app.broker.subscribe('app.started', onAppStart).setLimit(1);
 
   function onAppStart()
   {
+    watchDir();
+    readDir();
+  }
+
+  function watchDir()
+  {
     fs.watch(module.config.path, function(event, fileName)
     {
-      if (fileName !== null)
+      if (fileName === null)
       {
-        publishChangedMessage(fileName);
-      }
-    });
-
-    fs.readdir(module.config.path, function(err, fileNames)
-    {
-      if (err)
-      {
-        return module.error("Failed to read dir: %s", err.message);
+        return;
       }
 
-      fileNames.sort().forEach(publishChangedMessage);
+      if (readingDir)
+      {
+        readDirAgain = true;
+
+        return;
+      }
+
+      if (timerStartedAt === -1)
+      {
+        timerStartedAt = Date.now();
+      }
+
+      clearTimeout(timer);
+
+      if (Date.now() - timerStartedAt >= module.config.maxDelay)
+      {
+        return setImmediate(readDir);
+      }
+
+      timer = setTimeout(readDir, module.config.delay);
     });
   }
 
-  function publishChangedMessage(fileName)
+  function readDir()
   {
-    app.broker.publish('directoryWatcher.changed', createFileInfo(fileName));
+    readDirAgain = false;
+    readingDir = true;
+
+    fs.readdir(module.config.path, function(err, fileNames)
+    {
+      timer = null;
+      timerStartedAt = -1;
+      readingDir = false;
+
+      if (err)
+      {
+        module.error("Failed to read dir: %s", err.message);
+      }
+      else
+      {
+        fileNames
+          .map(createFileInfo)
+          .sort(function(a, b) { return a.timestamp - b.timestamp; })
+          .forEach(function(fileInfo) { app.broker.publish('directoryWatcher.changed', fileInfo); });
+      }
+
+      if (readDirAgain)
+      {
+        setImmediate(readDir);
+      }
+    });
   }
 
   function createFileInfo(fileName)
