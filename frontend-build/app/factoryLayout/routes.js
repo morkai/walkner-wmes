2c6b3ec897c2db413d3ea03da00a6448e8374503
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-09-01 03:32:09 +0200

    v0.40.0

--- frontend-build/app/factoryLayout/routes.js
+++ frontend-build/app/factoryLayout/routes.js
@@ -2,4 +2,4 @@
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 // Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
 
-define(["../user","../router","../viewport","i18n!app/nls/factoryLayout"],function(a,t,o){t.map("/factoryLayout",function(){o.loadPage(["app/factoryLayout/productionState","app/factoryLayout/pages/FactoryLayoutPage"],function(a,t){return new t({model:a})})}),t.map("/factoryLayout/prodLines",function(a){o.loadPage(["app/factoryLayout/productionState","app/factoryLayout/pages/ProdLineStateListPage"],function(t,o){return new o({model:t,rqlQuery:a.rql})})})});
+define(["../user","../router","../viewport","i18n!app/nls/factoryLayout"],function(t,o,a){o.map("/factoryLayout",function(){a.loadPage(["app/factoryLayout/productionState","app/factoryLayout/pages/FactoryLayoutPage"],function(t,o){return new o({model:t})})}),o.map("/factoryLayout/prodLines",function(t){a.loadPage(["app/factoryLayout/productionState","app/factoryLayout/ProdLineStateListOptions","app/factoryLayout/pages/ProdLineStateListPage"],function(o,a,e){return new e({model:o,listOptions:a.fromQuery(t.query)})})})});
