6df4a903f0b73a7c7a9c653e1686c8a7c09f2e72
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-09-14 13:02:02 +0200

    reports: clean up the select2 instances created by the settings view.

--- frontend/app/reports/views/ReportSettingsView.js
+++ frontend/app/reports/views/ReportSettingsView.js
@@ -81,6 +81,7 @@ define([
 
     destroy: function()
     {
+      this.$('.select2-offscreen[tabindex="-1"]').select2('destroy');
       this.$('.colorpicker-component').colorpicker('destroy');
     },
 
