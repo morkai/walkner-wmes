a96996f0522828adc6127ed4a8a1867dc2fd1c94
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-09-23 21:34:27 +0200

    directoryWatcher: moduleId property is now set to the module's name.

--- backend/modules/directoryWatcher.js
+++ backend/modules/directoryWatcher.js
@@ -103,7 +103,7 @@ exports.start = function startDirectoryWatcherModule(app, module)
   function createFileInfo(fileName)
   {
     var fileInfo = {
-      moduleId: module.id,
+      moduleId: module.name,
       timestamp: -1,
       fileName: fileName,
       filePath: path.resolve(module.config.path, fileName)
