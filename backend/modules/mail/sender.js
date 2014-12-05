e0daf71039b66fcd038ea6b2bc581f899f279cfb
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-12-06 00:21:04 +0100

    mail/sender: upgraded to Nodemailer v1.3.0 and added an ability to send e-mail through an HTTP gateway.

--- backend/modules/mail/sender.js
+++ backend/modules/mail/sender.js
@@ -5,23 +5,30 @@
 'use strict';
 
 var nodemailer = require('nodemailer');
+var request = require('request');
 
 exports.DEFAULT_CONFIG = {
-  controllerId: 'controller',
-  serviceType: null,
-  serviceOptions: {},
-  from: {tagName: 'mailer.from', default: 'someone@the.net'},
-  bcc: {tagName: 'mailer.bcc', default: ''},
-  replyTo: {tagName: 'mailer.replyTo', default: 'someone@the.net'}
+  smtp: null,
+  from: 'someone@the.net',
+  bcc: '',
+  replyTo: 'someone@the.net',
+  expressId: 'express',
+  secretKey: null,
+  remoteSenderUrl: null
 };
 
 exports.start = function startMailSenderModule(app, module)
 {
-  var config = module.config;
-  var transport = nodemailer.createTransport(
-    module.config.transport,
-    module.config.options
-  );
+  if (module.config.smtp && module.config.remoteSenderUrl)
+  {
+    throw new Error("`smtp` and `remoteSenderUrl` cannot be used at the same time!");
+  }
+  else if (!module.config.smtp && !module.config.remoteSenderUrl)
+  {
+    throw new Error("`smtp` or `remoteSenderUrl` must be set!");
+  }
+
+  var transport = module.config.smtp ? nodemailer.createTransport(module.config.smtp) : null;
 
   /**
    * @param {string|Array.<string>} to
@@ -31,17 +38,94 @@ exports.start = function startMailSenderModule(app, module)
    */
   module.send = function(to, subject, text, done)
   {
-    var controller = app[config.controllerId];
-    var values = controller && controller.values ? controller.values : {};
+    if (transport === null)
+    {
+      sendThroughRemote(to, subject, text, done);
+    }
+    else
+    {
+      sendThroughSmtp(to, subject, text, done);
+    }
+  };
+
+  function sendThroughRemote(to, subject, text, done)
+  {
+    var options = {
+      url: module.config.remoteSenderUrl,
+      method: 'POST',
+      json: true,
+      body: {
+        to: to,
+        subject: subject,
+        text: text,
+        secretKey: module.config.secretKey
+      }
+    };
+
+    request(options, function(err, res)
+    {
+      if (err)
+      {
+        return done(err);
+      }
+
+      if (res.statusCode !== 204)
+      {
+        return done(new Error('INVALID_REMOTE_RESPONSE'));
+      }
+
+      return done();
+    });
+  }
+
+  function sendThroughSmtp(to, subject, text, done)
+  {
     var mailOptions = {
-      from: String(values[config.from.tagName] || config.from.default),
+      from: String(module.config.from),
       to: Array.isArray(to) ? to.join(', ') : to,
-      bcc: String(values[config.bcc.tagName] || config.bcc.default),
-      replyTo: String(values[config.replyTo.tagName] || config.replyTo.default),
+      bcc: String(module.config.bcc),
+      replyTo: String(module.config.replyTo),
       subject: subject,
       text: String(text)
     };
 
     transport.sendMail(mailOptions, done);
-  };
+  }
+
+  app.onModuleReady(module.config.expressId, function()
+  {
+    var express = app[module.config.expressId];
+
+    express.options('/mail;send', function(req, res)
+    {
+      res.set('Access-Control-Allow-Origin', '*');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.end();
+    });
+
+    express.post('/mail;send', function(req, res, next)
+    {
+      res.set('Access-Control-Allow-Origin', '*');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+
+      if (module.config.secretKey !== null && req.body.secretKey !== module.config.secretKey)
+      {
+        return next(express.createHttpError('INVALID_SECRET_KEY', 401));
+      }
+
+      module.send(req.body.to, req.body.subject, req.body.text, function(err)
+      {
+        if (err)
+        {
+          module.error("Failed to send e-mail [%s] to [%s]: %s", req.body.subject, req.body.to, err.message);
+
+          return next(err);
+        }
+
+        module.debug("Sent e-mail to [%s]: %s", req.body.to, req.body.subject);
+
+        res.send(204);
+      });
+    });
+  });
 };
