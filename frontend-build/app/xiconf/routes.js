cf4a90a013c8c5d496478cbc2a2199d0a5ae5aa2
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-03-30 00:03:20 +0200

    v0.53.2

--- frontend-build/app/xiconf/routes.js
+++ frontend-build/app/xiconf/routes.js
@@ -2,4 +2,4 @@
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 // Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
 
-define(["../router","../viewport","../user","i18n!app/nls/xiconf"],function(n,e,i){var a=i.auth("XICONF:VIEW");n.map("/xiconf/results",a,function(n){e.loadPage(["app/xiconf/pages/XiconfResultListPage"],function(e){return new e({rql:n.rql})})}),n.map("/xiconf/results/:id",a,function(n){e.loadPage(["app/xiconf/pages/XiconfResultDetailsPage"],function(e){return new e({modelId:n.params.id})})})});
+define(["../router","../viewport","../user","i18n!app/nls/xiconf"],function(n,e,a){var i=a.auth("XICONF:VIEW");n.map("/xiconf/results",i,function(n){e.loadPage(["app/xiconf/pages/XiconfResultListPage"],function(e){return new e({rql:n.rql})})}),n.map("/xiconf/results/:id",i,function(n){e.loadPage(["app/xiconf/pages/XiconfResultDetailsPage"],function(e){return new e({modelId:n.params.id,tab:n.query.tab})})})});
