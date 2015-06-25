99b61283999909deed2d49dcfa498b79ef0a817b
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-06-25 10:09:37 +0200

    xiconf: changed the program synchronization process to be more asynchronous.

--- backend/modules/xiconf/routes/programs/sync.js
+++ backend/modules/xiconf/routes/programs/sync.js
@@ -34,8 +34,6 @@ module.exports = function syncProgramsRoute(app, xiconfModule, req, res, next)
     var res = args.res;
     var next = args.next;
 
-    var remotePrograms = programListToMap(req.body);
-
     step(
       function checkSocketStep()
       {
@@ -44,124 +42,79 @@ module.exports = function syncProgramsRoute(app, xiconfModule, req, res, next)
           return this.skip(new Error('SOCKET_CLOSED'));
         }
       },
+      function prepareRemoteProgramsStep()
+      {
+        this.remotePrograms = programListToMap(req.body);
+
+        return setImmediate(this.next());
+      },
       function findProgramsStep()
       {
         XiconfProgram.find().exec(this.next());
       },
-      function compareProgramsStep(err, programs)
+      function prepareLocalProgramsStep(err, programs)
       {
         if (err)
         {
           return this.skip(err);
         }
 
-        var localPrograms = programListToMap(programs);
-        var localCreatedPrograms = [];
-        var localUpdatedPrograms = [];
-        var localDeletedPrograms = [];
-        var remoteUpdatedPrograms = [];
-        var remoteDeletedPrograms = [];
+        this.localPrograms = programListToMap(programs);
 
-        _.forEach(remotePrograms, function(remoteProgram)
-        {
-          delete remotePrograms[remoteProgram._id];
-
-          if (!_.isArray(remoteProgram.steps))
-          {
-            remoteProgram.steps = tryJsonParseSteps(remoteProgram.steps);
-          }
-
-          var localProgram = localPrograms[remoteProgram._id];
-
-          if (!localProgram)
-          {
-            if (remoteProgram.deleted)
-            {
-              return;
-            }
-
-            localCreatedPrograms.push(new XiconfProgram(remoteProgram));
-
-            return;
-          }
-
-          delete localPrograms[remoteProgram._id];
-
-          var localUpdatedAt = localProgram.updatedAt.getTime();
-
-          if (remoteProgram.updatedAt === localUpdatedAt)
-          {
-            return;
-          }
-
-          if (remoteProgram.updatedAt > localUpdatedAt)
-          {
-            if (remoteProgram.deleted)
-            {
-              localProgram.updatedAt = remoteProgram.updatedAt;
-              localProgram.deleted = true;
-
-              localDeletedPrograms.push(localProgram);
-            }
-            else
-            {
-              localProgram.set(remoteProgram);
-
-              localUpdatedPrograms.push(localProgram);
-            }
-          }
-          else
-          {
-            if (localProgram.deleted)
-            {
-              remoteDeletedPrograms.push(localProgram);
-            }
-            else
-            {
-              remoteUpdatedPrograms.push(localProgram);
-            }
-          }
-        });
-
-        var remoteCreatedPrograms = _.values(localPrograms).filter(function(program) { return !program.deleted; });
-
+        return setImmediate(this.next());
+      },
+      function compareProgramsStep()
+      {
         this.remoteChanges = {
-          created: remoteCreatedPrograms,
-          updated: remoteUpdatedPrograms,
-          deleted: remoteDeletedPrograms
+          created: [],
+          updated: [],
+          deleted: []
         };
 
         this.localChanges = {
-          created: localCreatedPrograms,
-          updated: localUpdatedPrograms,
-          deleted: localDeletedPrograms
+          created: [],
+          updated: [],
+          deleted: []
         };
+
+        var remoteProgramIds = Object.keys(this.remotePrograms);
+
+        compareNextProgram(
+          remoteProgramIds,
+          this.remotePrograms,
+          this.localPrograms,
+          this.remoteChanges,
+          this.localChanges,
+          this.next()
+        );
+      },
+      function setRemoteCreatedProgramsStep()
+      {
+        this.remoteChanges.created = _.filter(
+          _.values(this.localPrograms),
+          function(program) { return !program.deleted; }
+        );
 
         setImmediate(this.next());
       },
-      function applyChangesStep(err)
+      function applyChangesStep()
       {
-        if (err)
-        {
-          return next(err);
-        }
-
         var localChanges = this.localChanges;
         var i;
 
         for (i = 0; i < localChanges.created.length; ++i)
         {
-          localChanges.created[i].save(this.parallel());
+          localChanges.created[i].save(this.group());
         }
 
         for (i = 0; i < localChanges.updated.length; ++i)
         {
-          localChanges.updated[i].save(this.parallel());
+          localChanges.updated[i].save(this.group());
         }
 
         for (i = 0; i < localChanges.deleted.length; ++i)
         {
-          localChanges.deleted[i].save(this.parallel());
+          localChanges.deleted[i].save(this.group());
         }
       },
       function sendResponseStep(err)
@@ -203,6 +156,11 @@ module.exports = function syncProgramsRoute(app, xiconfModule, req, res, next)
           }
         }
 
+        this.remoteChanges = null;
+        this.localChanges = null;
+        this.remotePrograms = null;
+        this.localPrograms = null;
+
         setImmediate(syncNext);
       }
     );
@@ -230,5 +188,86 @@ module.exports = function syncProgramsRoute(app, xiconfModule, req, res, next)
     });
 
     return programMap;
+  }
+
+  function compareNextProgram(remoteProgramIds, remotePrograms, localPrograms, remoteChanges, localChanges, done)
+  {
+    var remoteProgramId = remoteProgramIds.shift();
+
+    if (!remoteProgramId)
+    {
+      return done();
+    }
+
+    var remoteProgram = remotePrograms[remoteProgramId];
+
+    delete remotePrograms[remoteProgram._id];
+
+    if (!_.isArray(remoteProgram.steps))
+    {
+      remoteProgram.steps = tryJsonParseSteps(remoteProgram.steps);
+    }
+
+    var localProgram = localPrograms[remoteProgram._id];
+
+    if (!localProgram)
+    {
+      if (!remoteProgram.deleted)
+      {
+        localChanges.created.push(new XiconfProgram(remoteProgram));
+      }
+
+      return setImmediate(
+        compareNextProgram,
+        remoteProgramIds,
+        remotePrograms,
+        localPrograms,
+        remoteChanges,
+        localChanges,
+        done
+      );
+    }
+
+    delete localPrograms[remoteProgram._id];
+
+    var localUpdatedAt = localProgram.updatedAt.getTime();
+
+    if (remoteProgram.updatedAt > localUpdatedAt)
+    {
+      if (remoteProgram.deleted)
+      {
+        localProgram.updatedAt = remoteProgram.updatedAt;
+        localProgram.deleted = true;
+
+        localChanges.deleted.push(localProgram);
+      }
+      else
+      {
+        localProgram.set(remoteProgram);
+
+        localChanges.updated.push(localProgram);
+      }
+    }
+    else if (remoteProgram.updatedAt !== localUpdatedAt)
+    {
+      if (localProgram.deleted)
+      {
+        remoteChanges.deleted.push(localProgram);
+      }
+      else
+      {
+        remoteChanges.updated.push(localProgram);
+      }
+    }
+
+    return setImmediate(
+      compareNextProgram,
+      remoteProgramIds,
+      remotePrograms,
+      localPrograms,
+      remoteChanges,
+      localChanges,
+      done
+    );
   }
 };
