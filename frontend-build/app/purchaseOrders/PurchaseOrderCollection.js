cadea6345c4e157678ed47f817e2e08f60aecc0c
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-06-30 00:01:28 +0200

    v0.63.5

--- frontend-build/app/purchaseOrders/PurchaseOrderCollection.js
+++ frontend-build/app/purchaseOrders/PurchaseOrderCollection.js
@@ -2,4 +2,4 @@
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 // Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
 
-define(["../time","../core/Collection","./PurchaseOrder"],function(e,r,t){"use strict";return r.extend({model:t,rqlQuery:function(r){var t=e.getMoment().utc().hours(0).minutes(0).seconds(0).milliseconds(0).add(1,"days").valueOf();return r.Query.fromObject({fields:{changes:0,"items.prints":0},limit:20,sort:{scheduledAt:1},selector:{name:"and",args:[{name:"eq",args:["open",!0]},{name:"lt",args:["scheduledAt",t]},{name:"populate",args:["vendor"]}]}})}})});
+define(["../time","../core/Collection","./PurchaseOrder"],function(e,r,n){"use strict";return r.extend({model:n,rqlQuery:function(e){return e.Query.fromObject({fields:{changes:0,"items.prints":0},limit:20,sort:{scheduledAt:1},selector:{name:"and",args:[{name:"eq",args:["open",!0]},{name:"populate",args:["vendor"]}]}})}})});
