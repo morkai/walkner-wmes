d321902e0a42b7f904e6f77868f802f355556daa
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-08-01 23:40:17 +0200

    v0.67.0

--- frontend-build/app/xiconf/util/serializeXiconfLicenseFeatures.js
+++ frontend-build/app/xiconf/util/serializeXiconfLicenseFeatures.js
@@ -2,4 +2,4 @@
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 // Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
 
-define(["app/licenses/util/createLicenseFeaturesSerializer"],function(e){"use strict";return e(["wmes","sol","t24vdc","led","gprs","glp2"])});
+define(["app/licenses/util/createLicenseFeaturesSerializer"],function(e){"use strict";return e(["wmes","sol","t24vdc","led","gprs","glp2","fl"])});
