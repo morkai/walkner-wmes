ceedb4868952ba9b14a29d59e0dd02415e5b53b3
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-09-28 12:53:34 +0200

    wmes-importer-sap: changed the order/oper step count from 11 to 13.

--- config/wmes-importer-sap.js
+++ config/wmes-importer-sap.js
@@ -108,28 +108,28 @@ exports.directoryWatcher = {
 };
 
 exports.currentDayOrderImporter = {
-  orderStepCount: 11,
-  operStepCount: 11,
+  orderStepCount: 13,
+  operStepCount: 13,
   parsedOutputDir: IMPORT_OUTPUT_DIR
 };
 
 exports.nextDayOrderImporter = {
-  orderStepCount: 11,
-  operStepCount: 11,
+  orderStepCount: 13,
+  operStepCount: 13,
   filterRe: /^Job PL02_(ORDER|OPER)_INFO_2D, Step ([0-9]+)\.html?$/,
   parsedOutputDir: IMPORT_OUTPUT_DIR
 };
 
 exports.prevDayOrderImporter = {
-  orderStepCount: 11,
-  operStepCount: 11,
+  orderStepCount: 13,
+  operStepCount: 13,
   filterRe: /^Job PL02_(ORDER|OPER)_INFO_3D, Step ([0-9]+)\.html?$/,
   parsedOutputDir: IMPORT_OUTPUT_DIR
 };
 
 exports.emptyOrderImporter = {
-  orderStepCount: 11,
-  operStepCount: 11,
+  orderStepCount: 13,
+  operStepCount: 13,
   parsedOutputDir: IMPORT_OUTPUT_DIR
 };
 
