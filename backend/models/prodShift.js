15d702d3dacddf394fca08617d44eb8c1cb17fec
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-11-22 16:38:49 +0100

    prodShifts: replaced the `date_-1_prodLine_1` index with the `prodLine_1_date_-1` index and added a new one: `date_-1` .

--- backend/models/prodShift.js
+++ backend/models/prodShift.js
@@ -106,12 +106,13 @@ module.exports = function setupProdShiftModel(app, mongoose)
 
   prodShiftSchema.statics.TOPIC_PREFIX = 'prodShifts';
 
+  prodShiftSchema.index({date: -1});
   prodShiftSchema.index({division: 1, date: -1});
   prodShiftSchema.index({subdivision: 1, date: -1});
   prodShiftSchema.index({mrpController: 1, date: -1});
   prodShiftSchema.index({prodFlow: 1, date: -1});
   prodShiftSchema.index({workCenter: 1, date: -1});
-  prodShiftSchema.index({date: -1, prodLine: 1});
+  prodShiftSchema.index({prodLine: 1, date: -1});
   prodShiftSchema.index({shutdown: 1, prodLine: 1});
   prodShiftSchema.index({shutdown: 1, date: -1});
 
