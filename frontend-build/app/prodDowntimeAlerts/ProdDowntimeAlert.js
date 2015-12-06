d8feb1a438d8586bab0bc3864c55aca3c3e90243
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-12-06 14:21:09 +0100

    v0.76.0

--- frontend-build/app/prodDowntimeAlerts/ProdDowntimeAlert.js
+++ frontend-build/app/prodDowntimeAlerts/ProdDowntimeAlert.js
@@ -2,4 +2,4 @@
 // Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
 // Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
 
-define(["../core/Model"],function(e){"use strict";return e.extend({urlRoot:"/prodDowntimeAlerts",clientUrlRoot:"#prodDowntimeAlerts",topicPrefix:"prodDowntimeAlerts",privilegePrefix:"PROD_DOWNTIME_ALERTS",nlsDomain:"prodDowntimeAlerts",labelAttribute:"subject"},{CONDITION_TYPES:["reason","aor","division","subdivision","mrpController","prodFlow","workCenter","prodLine"]})});
+define(["../core/Model"],function(e){"use strict";return e.extend({urlRoot:"/prodDowntimeAlerts",clientUrlRoot:"#prodDowntimeAlerts",topicPrefix:"prodDowntimeAlerts",privilegePrefix:"PROD_DOWNTIME_ALERTS",nlsDomain:"prodDowntimeAlerts",labelAttribute:"name"},{CONDITION_TYPES:["reason","aor","division","subdivision","mrpController","prodFlow","workCenter","prodLine"]})});
