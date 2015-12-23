1d11b780961975f3e04ad37e14cbc6188dcaa344
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-12-23 13:49:28 +0100

    cags: remove CAGs used in a newly added or edited group from all other existing groups so that a single CAG can belong to only one group.

--- backend/modules/cags/index.js
+++ backend/modules/cags/index.js
@@ -23,4 +23,37 @@ exports.start = function startCagsModule(app, module)
     ],
     setUpRoutes.bind(null, app, module)
   );
+
+  app.broker.subscribe('cagGroups.added', onCagGroupChanged);
+  app.broker.subscribe('cagGroups.edited', onCagGroupChanged);
+
+  function onCagGroupChanged(message)
+  {
+    var mongoose = app[module.config.mongooseId];
+
+    if (!mongoose)
+    {
+      return;
+    }
+
+    var CagGroup = mongoose.model('CagGroup');
+    var conditions = {
+      _id: {
+        $ne: message.model._id
+      }
+    };
+    var update = {
+      $pullAll: {
+        cags: message.model.cags
+      }
+    };
+
+    CagGroup.update(conditions, update, {multi: true}, function(err)
+    {
+      if (err)
+      {
+        module.error("Failed to update CAGs in groups: %s", err.message);
+      }
+    });
+  }
 };
