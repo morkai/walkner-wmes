60e29ea79dcbdddc7aecc9471204b06d57a7c79d
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-01-05 15:35:07 +0100

    scripts: added `excludeCollections` config option to the `backup-db` script.

--- bin/backup-db.js
+++ bin/backup-db.js
@@ -1,88 +1,134 @@
-// Copyright (c) 2014, Łukasz Walukiewicz <lukasz@walukiewicz.eu>. Some Rights Reserved.
-// Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
-// Part of the walkner-wmes project <http://lukasz.walukiewicz.eu/p/walkner-wmes>
+// Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
 'use strict';
 
-var startTime = Date.now();
-
 var format = require('util').format;
-var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
+var path = require('path');
+var fs = require('fs');
 var config = require(process.argv[2]);
+var startTime = new Date();
 
-clean(dump);
+console.log("Removing old dump directory: %s...", config.backupPath);
 
-function clean(next)
+try
 {
-  exec(format('rmdir /S /Q "%s"', config.backupPath), function(err)
+  execSync(format('rmdir /S /Q "%s"', config.backupPath));
+}
+catch (err) {}
+
+console.log("Removing old backup files...");
+
+var backupRootPath = path.dirname(config.backupPath);
+var oldBackupFiles = fs.readdirSync(backupRootPath)
+  .map(function(file)
   {
-    if (err)
+    var matches = file.match(/^dump-([0-9]{4})-([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{2})/);
+
+    if (matches)
     {
-      console.error("Failed to clean the dump: %s", err.message);
-    }
-    else
-    {
-      console.log("Cleaned the dump in %s ms!", Date.now() - startTime);
+      return {
+        name: file,
+        path: path.join(backupRootPath, file),
+        time: new Date(+matches[1], +matches[2] - 1, +matches[3], +matches[4], +matches[5], +matches[6], 0)
+      };
     }
 
-    if (next)
-    {
-      next();
-    }
+    return null;
+  })
+  .filter(function(file)
+  {
+    return !!file;
+  })
+  .sort(function(a, b)
+  {
+    return a.time - b.time;
   });
+
+for (var i = 0, l = oldBackupFiles.length - 3; i < l; ++i)
+{
+  var oldBackupFile = oldBackupFiles[i];
+
+  console.log("...%s", oldBackupFile.name);
+
+  try
+  {
+    fs.unlinkSync(oldBackupFile.path);
+  }
+  catch (err)
+  {
+    console.log("Failed to remove old backup file: %s", err.message);
+  }
 }
 
-function dump()
+var excludeCollections = typeof config.excludeCollections === 'function'
+  ? config.excludeCollections(startTime)
+  : config.excludeCollections;
+
+if (!Array.isArray(excludeCollections))
 {
-  var cmd = format(
-    '"%s" --db "%s" --out "%s"',
-    config.mongodumpExe,
-    config.dbName,
-    config.backupPath
-  );
-
-  exec(cmd, function(err)
-  {
-    if (err)
-    {
-      console.error("Failed to dump the database: %s", err.message);
-      process.exit(1);
-    }
-
-    console.log("Dumped the database in %s ms!", Date.now() - startTime);
-
-    pack();
-  });
+  excludeCollections = [];
 }
 
-function pack()
+var dumpCmd = format(
+  '"%s" --db "%s" --out "%s"',
+  config.mongodumpExe,
+  config.dbName,
+  config.backupPath
+);
+
+excludeCollections.forEach(function(collectionName)
 {
-  var now = new Date();
-  var date = now.getFullYear()
-    + '-' + pad0(now.getMonth() + 1)
-    + '-' + pad0(now.getDate())
-    + '-' + pad0(now.getHours())
-    + '-' + pad0(now.getMinutes())
-    + '-' + pad0(now.getSeconds());
-  var zipPath = config.backupPath + '-' + date + '.7z';
+  dumpCmd += format(' --excludeCollection "%s"', collectionName);
+});
 
-  var cmd = format('"%s" a -t7z "%s" "%s"', config.zipExe, zipPath, config.backupPath);
+console.log("Dumping the database...");
+console.log(dumpCmd);
 
-  exec(cmd, function(err)
-  {
-    clean();
+startTime = new Date();
 
-    if (err)
-    {
-      console.error("Failed to pack the dump: %s", err.message);
-      process.exit(1);
-    }
-    else
-    {
-      console.log("Packed the dump in %s ms!", Date.now() - startTime);
-    }
-  });
+try
+{
+  execSync(dumpCmd);
 }
+catch (err)
+{
+  process.exit(1);
+}
+
+console.log("...done in %ss", (Date.now() - startTime) / 1000);
+
+startTime = new Date();
+
+var date = startTime.getFullYear()
+  + '-' + pad0(startTime.getMonth() + 1)
+  + '-' + pad0(startTime.getDate())
+  + '-' + pad0(startTime.getHours())
+  + '-' + pad0(startTime.getMinutes())
+  + '-' + pad0(startTime.getSeconds());
+var archivePath = config.backupPath + '-' + date + '.7z';
+
+console.log("Packing to: %s...", archivePath);
+
+try
+{
+  execSync(format('"%s" a -t7z "%s" "%s"', config.zipExe, archivePath, config.backupPath));
+}
+catch (err)
+{
+  console.log("Failed to pack: %s", err.message);
+  process.exit(1);
+}
+
+console.log("...done in %ss", (Date.now() - startTime) / 1000);
+
+console.log("Removing new dump directory...", config.backupPath);
+
+try
+{
+  execSync(format('rmdir /S /Q "%s"', config.backupPath));
+}
+catch (err) {}
 
 function pad0(num)
 {
--- config/backup.js
+++ config/backup.js
@@ -1,3 +1,5 @@
+/*jshint -W098*/
+
 'use strict';
 
 exports.dbName = 'walkner-wmes';
@@ -7,3 +9,8 @@ exports.backupPath = 'C:/backups/dump';
 exports.mongodumpExe = 'C:/Program Files/MongoDB/bin/mongodump.exe';
 
 exports.zipExe = 'C:/Program Files/7-Zip/7z.exe';
+
+exports.excludeCollections = function(date)
+{
+  return [];
+};
