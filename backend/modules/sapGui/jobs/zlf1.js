da9f8bd1adafc6c862d142e5c60feda123670235
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-01-05 14:24:27 +0100

    sapGui: zlf1: added an optional files move step after a successful transaction.

--- backend/modules/sapGui/jobs/zlf1.js
+++ backend/modules/sapGui/jobs/zlf1.js
@@ -2,7 +2,28 @@
 
 'use strict';
 
-module.exports = function runDocsJob(app, sapGuiModule, job, done)
+var format = require('util').format;
+var exec = require('child_process').exec;
+var _ = require('lodash');
+
+module.exports = function runZlf1Job(app, sapGuiModule, job, done)
 {
-  sapGuiModule.runScript(job, 'T_ZLF1.exe', [], done);
+  sapGuiModule.runScript(job, 'T_ZLF1.exe', [], function(err, exitCode, output)
+  {
+    if (_.includes(output, 'bytes transmitted')
+      && !_.isEmpty(job.sourceDestination)
+      && !_.isEmpty(job.targetDestination))
+    {
+      var cmd = format('MOVE /Y "%s" "%s"', job.sourceDestination, job.targetDestination);
+
+      exec(cmd, function(err, stdout, stderr)
+      {
+        done(err, exitCode, format("%s\r\nMOVE (stdout):\r\n%s\r\nMOVE (stderr):\r\n", output, stdout, stderr));
+      });
+    }
+    else
+    {
+      done(err, exitCode, output);
+    }
+  });
 };
