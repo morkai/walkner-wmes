d53eaf18480b54e9c58c0ef2d1b60de6063653f6
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-01-14 20:51:03 +0100

    reports: lean: improved the `Production lines' filter to show only lines matching the 'From', 'To', 'Divisions' and 'Subdivision types' filters.

--- frontend/app/reports/views/8/FilterView.js
+++ frontend/app/reports/views/8/FilterView.js
@@ -2,7 +2,9 @@
 
 define([
   'underscore',
+  'jquery',
   'form2js',
+  'select2',
   'app/i18n',
   'app/time',
   'app/viewport',
@@ -13,7 +15,9 @@ define([
   'app/reports/templates/8/filter'
 ], function(
   _,
+  $,
   form2js,
+  select2,
   t,
   time,
   viewport,
@@ -31,7 +35,39 @@ define([
 
     events: _.extend({
 
+      'change #-from': 'setUpProdLineSelect2',
+      'change #-to': 'setUpProdLineSelect2',
+      'change [name="divisions[]"]': 'setUpProdLineSelect2',
+      'change [name="subdivisionTypes[]"]': 'setUpProdLineSelect2'
+
     }, FilterView.prototype.events),
+
+    initialize: function()
+    {
+      FilterView.prototype.initialize.apply(this, arguments);
+
+      var subdivisionToProdLines = {};
+
+      orgUnits.getAllByType('prodLine').forEach(function(prodLine)
+      {
+        var prodLineUnits = orgUnits.getAllForProdLine(prodLine);
+
+        if (!subdivisionToProdLines[prodLineUnits.subdivision])
+        {
+          subdivisionToProdLines[prodLineUnits.subdivision] = [];
+        }
+
+        var deactivatedAt = prodLine.get('deactivatedAt');
+
+        subdivisionToProdLines[prodLineUnits.subdivision].push({
+          id: prodLine.id,
+          text: prodLine.get('description'),
+          deactivatedAt: deactivatedAt ? Date.parse(deactivatedAt) : 0
+        });
+      });
+
+      this.subdivisionToProdLines = subdivisionToProdLines;
+    },
 
     serialize: function()
     {
@@ -47,11 +83,99 @@ define([
     {
       FilterView.prototype.afterRender.call(this);
 
+      this.setUpProdLineSelect2();
+    },
+
+    setUpProdLineSelect2: function()
+    {
+      var timeRange = this.serializeTimeRange();
+      var selectedDivisions = this.getCheckboxValues('[name="divisions[]"]:checked');
+      var selectedSubdivisionTypes = this.getCheckboxValues('[name="subdivisionTypes[]"]:checked');
+
+      if (!selectedDivisions.length)
+      {
+        selectedDivisions = this.getCheckboxValues('[name="divisions[]"]');
+      }
+
+      if (!selectedSubdivisionTypes.length)
+      {
+        selectedSubdivisionTypes = this.getCheckboxValues('[name="subdivisionTypes[]"]');
+      }
+
+      var subdivisionToProdLines = this.subdivisionToProdLines;
+      var data = [];
+
+      _.forEach(selectedDivisions, function(divisionId)
+      {
+        var division = orgUnits.getByTypeAndId('division', divisionId);
+        var subdivisions = orgUnits.getChildren(division).filter(function(subdivision)
+        {
+          return _.includes(selectedSubdivisionTypes, subdivision.get('type'));
+        });
+
+        _.forEach(subdivisions, function(subdivision)
+        {
+          var item = {
+            text: division.id + ' \\ ' + subdivision.getLabel(),
+            children: []
+          };
+
+          _.forEach(subdivisionToProdLines[subdivision.id], function(prodLine)
+          {
+            if (!prodLine.deactivatedAt || prodLine.deactivatedAt >= timeRange.from)
+            {
+              item.children.push(prodLine);
+            }
+          });
+
+          if (item.children.length)
+          {
+            data.push(item);
+          }
+        });
+      });
+
+      var matcher = $().select2.defaults.matcher;
+
       this.$id('prodLines').select2({
         width: 322,
         multiple: true,
-        data: orgUnits.getAllByType('prodLine').map(idAndLabel)
+        data: data.length === 1 ? data[0].children : data,
+        dropdownCssClass: 'reports-8-prodLines-select2',
+        matcher: function(term, text, option)
+        {
+          return matcher(term, option.id) || matcher(term, text);
+        },
+        formatResult: function(item, $container, query, e)
+        {
+          if (!item.id)
+          {
+            return e(item.text);
+          }
+
+          var html = [];
+
+          select2.util.markMatch(item.id, query.term, html, e);
+
+          html.push(': ');
+          html.push('<span style="text-decoration: ' + (item.deactivatedAt ? 'line-through' : 'initial') + '">');
+          select2.util.markMatch(item.text, query.term, html, e);
+          html.push('</span>');
+
+          return html.join('');
+        },
+        formatSelection: function(item)
+        {
+          return item.deactivatedAt
+            ? ('<span style="text-decoration: line-through">' + item.id + '</span>')
+            : item.id;
+        }
       });
+    },
+
+    getCheckboxValues: function(selector)
+    {
+      return this.$(selector).get().map(function(d) { return d.value; });
     },
 
     serializeQueryToForm: function()
@@ -83,19 +207,13 @@ define([
 
     serializeFormToQuery: function()
     {
-      var query = _.extend(_.result(this.model, 'defaults'), form2js(this.el), {
-        _rnd: Math.random()
-      });
-      var fromTime = time.getMoment(query.from, 'YYYY-MM-DD').startOf(query.interval).valueOf();
-      var toMoment = time.getMoment(query.to, 'YYYY-MM-DD').startOf(query.interval);
+      var query = _.extend(
+        _.result(this.model, 'defaults'),
+        form2js(this.el),
+        {_rnd: Math.random()}
+      );
 
-      if (fromTime === toMoment.valueOf())
-      {
-        toMoment.add(1, query.interval);
-      }
-
-      query.from = fromTime;
-      query.to = toMoment.valueOf();
+      _.extend(query, this.serializeTimeRange(query));
 
       this.$id('from').val(time.format(query.from, 'YYYY-MM-DD'));
       this.$id('to').val(time.format(query.to, 'YYYY-MM-DD'));
@@ -105,6 +223,31 @@ define([
       delete query.visibleSeries;
 
       return query;
+    },
+
+    serializeTimeRange: function(query)
+    {
+      if (!query)
+      {
+        query = {
+          from: this.$id('from').val(),
+          to: this.$id('to').val(),
+          interval: this.$('[name="interval"]:checked').val()
+        };
+      }
+
+      var fromTime = time.getMoment(query.from, 'YYYY-MM-DD').startOf(query.interval).valueOf();
+      var toMoment = time.getMoment(query.to, 'YYYY-MM-DD').startOf(query.interval);
+
+      if (fromTime === toMoment.valueOf())
+      {
+        toMoment.add(1, query.interval);
+      }
+
+      return {
+        from: fromTime,
+        to: toMoment.valueOf()
+      };
     }
 
   });
