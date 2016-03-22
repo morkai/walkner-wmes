e2bfbb43ced919883b81bc2b1f394d7885f2322a
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-03-22 19:58:13 +0100

    orders: document 15NCs can now be clicked to open the document file.

--- frontend/app/orders/views/DocumentListView.js
+++ frontend/app/orders/views/DocumentListView.js
@@ -1,9 +1,11 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
 define([
+  'app/user',
   'app/core/View',
   'app/orders/templates/documentList'
 ], function(
+  user,
   View,
   template
 ) {
@@ -13,10 +15,41 @@ define([
 
     template: template,
 
+    events: {
+      'click a': function(e)
+      {
+        var aEl = e.currentTarget;
+
+        if (aEl.dataset.checking)
+        {
+          return false;
+        }
+
+        var windowName = 'DOC_' + aEl.textContent.trim();
+
+        if (!aEl.dataset.checked)
+        {
+          this.tryOpenDocument(aEl, windowName);
+
+          return false;
+        }
+
+        if (e.ctrlKey)
+        {
+          return true;
+        }
+
+        window.open(aEl.href, 'DOC_' + windowName);
+
+        return false;
+      }
+    },
+
     serialize: function()
     {
       return {
-        documents: this.model.get('documents').toJSON()
+        documents: this.model.get('documents').toJSON(),
+        canView: user.isAllowedTo('LOCAL', 'DOCUMENTS:VIEW')
       };
     },
 
@@ -28,6 +61,26 @@ define([
     afterRender: function()
     {
       this.listenToOnce(this.model, 'change:documents', this.render);
+    },
+
+    tryOpenDocument: function(aEl, windowName)
+    {
+      aEl.dataset.checking = 1;
+
+      this.ajax({method: 'HEAD', url: aEl.href})
+        .fail(function()
+        {
+          aEl.parentElement.textContent = aEl.textContent;
+        })
+        .done(function()
+        {
+          window.open(aEl.href, 'DOC_' + windowName);
+        })
+        .always(function()
+        {
+          aEl.dataset.checking = '';
+          aEl.dataset.checked = 1;
+        });
     }
 
   });
