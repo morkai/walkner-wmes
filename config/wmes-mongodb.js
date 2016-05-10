282f534bd22a0962375be0b5f3c142bdd5b8be01
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-10 18:56:17 +0200

    wmes-mongodb: changed the connection configuration to avoid the 'topology destruction'.

--- config/wmes-mongodb.js
+++ config/wmes-mongodb.js
@@ -2,7 +2,7 @@
 
 const url = require('url');
 
-const uri = url.parse(process.env.WMES_MONGODB_URI || 'mongodb://hello:pass@127.0.0.1:27017/walkner-wmes');
+const uri = url.parse(process.env.WMES_MONGODB_URI || 'mongodb://127.0.0.1:27017/walkner-wmes');
 
 if (process.env.WMES_MONGODB_USER)
 {
@@ -16,12 +16,21 @@ if (process.env.WMES_MONGODB_USER)
 
 module.exports = {
   uri: url.format(uri),
+  keepAliveQueryInterval: 15000,
   server: {
-    poolSize: 15
+    poolSize: 15,
+    reconnectTries: Number.MAX_SAFE_INTEGER,
+    reconnectInterval: 1000,
+    socketOptions: {
+      autoReconnect: true,
+      keepAlive: 1000,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 0
+    }
   },
   db: {
     w: 1,
-    wtimeout: 1000,
+    wtimeout: 5000,
     nativeParser: true,
     forceServerObjectId: false
   }
