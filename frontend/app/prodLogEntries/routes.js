d77aff40d99059e44a606dceb66b6d620c3f204f
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-11 18:51:59 +0200

    prodLogEntries: fixed the missing translations.

--- frontend/app/prodLogEntries/routes.js
+++ frontend/app/prodLogEntries/routes.js
@@ -16,7 +16,13 @@ define([
   router.map('/prodLogEntries', canView, function(req)
   {
     viewport.loadPage(
-      ['app/prodLogEntries/pages/ProdLogEntryListPage', 'i18n!app/nls/prodLogEntries'],
+      [
+        'app/prodLogEntries/pages/ProdLogEntryListPage',
+        'i18n!app/nls/prodLogEntries',
+        'i18n!app/nls/prodShifts',
+        'i18n!app/nls/prodShiftOrders',
+        'i18n!app/nls/prodDowntimes'
+      ],
       function(ProdLogEntryListPage)
       {
         return new ProdLogEntryListPage({rql: req.rql});
