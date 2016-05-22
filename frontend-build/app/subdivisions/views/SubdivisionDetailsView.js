707b4adf3e3fee2652afc1af5f7a218093e21f26
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-22 18:25:04 +0200

    v0.87.0

--- frontend-build/app/subdivisions/views/SubdivisionDetailsView.js
+++ frontend-build/app/subdivisions/views/SubdivisionDetailsView.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/core/views/DetailsView","./decorateSubdivision","app/subdivisions/templates/details"],function(e,i,s){"use strict";return e.extend({template:s,localTopics:{"divisions.synced":"render"},serialize:function(){return{model:i(this.model)}}})});
+define(["app/core/views/DetailsView","./decorateSubdivision","app/subdivisions/templates/details"],function(e,i,s){"use strict";return e.extend({template:s,localTopics:{"divisions.synced":"render"},serialize:function(){return{idPrefix:this.idPrefix,model:i(this.model)}}})});
