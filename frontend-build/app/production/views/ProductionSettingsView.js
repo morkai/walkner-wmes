0721be7b81282e61845a2a3d7cf630eca0e6735b
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-24 01:16:38 +0200

    v0.88.0

---
+++ frontend-build/app/production/views/ProductionSettingsView.js
@@ -0,0 +1,3 @@
+// Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
+
+define(["app/settings/views/SettingsView","app/production/templates/settings"],function(t,e){"use strict";return t.extend({clientUrl:"#production;settings",template:e})});
--- frontend-build/app/subdivisions/Subdivision.js
+++ frontend-build/app/subdivisions/Subdivision.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Model"],function(i){"use strict";return i.extend({urlRoot:"/subdivisions",clientUrlRoot:"#subdivisions",topicPrefix:"subdivisions",privilegePrefix:"DICTIONARIES",nlsDomain:"subdivisions",labelAttribute:"name",defaults:{division:null,type:"assembly",name:null,prodTaskTags:null,aor:null,autoDowntime:null}})});
+define(["../core/Model"],function(i){"use strict";return i.extend({urlRoot:"/subdivisions",clientUrlRoot:"#subdivisions",topicPrefix:"subdivisions",privilegePrefix:"DICTIONARIES",nlsDomain:"subdivisions",labelAttribute:"name",defaults:{division:null,type:"assembly",name:null,prodTaskTags:null,aor:null,autoDowntime:null,initialDowntime:null}})});
