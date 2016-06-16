7e5d1f23c1e3182c056b7fb35c10edad494abca3
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-06-16 12:53:47 +0200

    Updated the frontend build.

--- frontend-build/app/isa/IsaLineStateCollection.js
+++ frontend-build/app/isa/IsaLineStateCollection.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Collection","./IsaLineState"],function(t,e){"use strict";return t.extend({model:e,parse:function(t){return t.collection.map(e.parse)},comparator:function(t,e){return null===t.attributes.requestedAt?1:null===e.attributes.requestedAt?-1:t.attributes.requestedAt-e.attributes.requestedAt}})});
+define(["../core/Collection","./IsaLineState"],function(e,t){"use strict";return e.extend({model:t,parse:function(e){return e.collection.map(t.parse)},comparator:function(e,t){return e=e.attributes,t=t.attributes,null===e.requestedAt?1:null===t.requestedAt?-1:e.requestType!==t.requestType?"delivery"===e.requestType?-1:1:e.requestedAt-t.requestedAt}})});
