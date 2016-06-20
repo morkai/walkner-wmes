9eb64d693000aecb23b6be89c474569194aa1c6a
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-06-20 19:03:46 +0200

    watchdog: added a ping check.

--- backend/modules/watchdog/index.js
+++ backend/modules/watchdog/index.js
@@ -2,48 +2,62 @@
 
 'use strict';
 
-var setUpNoEventCheck = require('./noEventCheck');
-var setUpAppStartedEventCheck = require('./appStartedEventCheck');
-var setUpEmptyDirectoryCheck = require('./emptyDirectoryCheck');
-var setUpDbCheck = require('./dbCheck');
+const setUpNoEventCheck = require('./noEventCheck');
+const setUpAppStartedEventCheck = require('./appStartedEventCheck');
+const setUpEmptyDirectoryCheck = require('./emptyDirectoryCheck');
+const setUpDbCheck = require('./dbCheck');
+const setUpPing = require('./ping');
 
 exports.DEFAULT_CONFIG = {
   mongooseId: 'mongoose',
   mailSenderId: 'mail/sender',
   smsSenderId: 'sms/sender',
   twilioId: 'twilio',
+  expressId: 'express',
   recipients: [],
   appStartedRecipients: [],
   appStartedCallRecipient: null,
   noEventRecipients: [],
   events: [],
-  emptyDirectories: []
+  emptyDirectories: [],
+  ping: {
+    secretKey: '?',
+    remoteUrl: 'http://127.0.0.1/watchdog/ping',
+    localUrl: 'http://127.0.0.1/ping',
+    interval: 0,
+    window: 0
+  }
 };
 
 exports.start = function startWatchdogModule(app, watchdogModule)
 {
-  app.onModuleReady(
-    [
-      watchdogModule.config.mongooseId,
-      watchdogModule.config.mailSenderId
-    ],
-    setUpNoEventCheck.bind(null, app, watchdogModule)
-  );
+  if (!watchdogModule.config.ping.window)
+  {
+    app.onModuleReady(
+      [
+        watchdogModule.config.mongooseId,
+        watchdogModule.config.mailSenderId
+      ],
+      setUpNoEventCheck.bind(null, app, watchdogModule)
+    );
 
-  app.onModuleReady(
-    [
-      watchdogModule.config.mongooseId,
-      watchdogModule.config.mailSenderId
-    ],
-    setUpAppStartedEventCheck.bind(null, app, watchdogModule)
-  );
+    app.onModuleReady(
+      [
+        watchdogModule.config.mongooseId,
+        watchdogModule.config.mailSenderId
+      ],
+      setUpAppStartedEventCheck.bind(null, app, watchdogModule)
+    );
 
-  app.onModuleReady(
-    [
-      watchdogModule.config.mongooseId
-    ],
-    setUpDbCheck.bind(null, app, watchdogModule)
-  );
+    app.onModuleReady(
+      [
+        watchdogModule.config.mongooseId
+      ],
+      setUpDbCheck.bind(null, app, watchdogModule)
+    );
 
-  setUpEmptyDirectoryCheck(app, watchdogModule);
+    setUpEmptyDirectoryCheck(app, watchdogModule);
+  }
+
+  setUpPing(app, watchdogModule);
 };
