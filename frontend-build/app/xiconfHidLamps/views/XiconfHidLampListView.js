96f31502bc3d296746da91bcde7b1d84cd5fe21b
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-07-10 13:16:13 +0200

    Updated the frontend build.

---
+++ frontend-build/app/xiconfHidLamps/views/XiconfHidLampListView.js
@@ -0,0 +1,3 @@
+// Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
+
+define(["app/core/views/ListView"],function(i){"use strict";return i.extend({className:"is-clickable",columns:[{id:"_id",className:"is-min"},{id:"nc12",className:"is-min"},"description"]})});
