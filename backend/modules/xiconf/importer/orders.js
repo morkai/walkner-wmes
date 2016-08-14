47e2003ad453289e270edfc763b9f46d91933b82
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-08-14 12:55:32 +0200

    xiconf/importer/orders: override existing imported files.

--- backend/modules/xiconf/importer/orders.js
+++ backend/modules/xiconf/importer/orders.js
@@ -383,7 +383,7 @@ exports.start = function startXiconfOrdersImporterModule(app, module)
   {
     var newFilePath = path.join(module.config.parsedOutputDir, path.basename(oldFilePath));
 
-    fs.move(oldFilePath, newFilePath, function(err)
+    fs.move(oldFilePath, newFilePath, {clobber: true}, function(err)
     {
       if (err)
       {
