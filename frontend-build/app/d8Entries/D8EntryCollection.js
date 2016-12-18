be4f6af7dcd1f0dbd1c0c149fd5a5ff7e022bad8
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-12-18 22:26:20 +0100

    v0.101.1

--- frontend-build/app/d8Entries/D8EntryCollection.js
+++ frontend-build/app/d8Entries/D8EntryCollection.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Collection","./D8Entry"],function(e,t){"use strict";return e.extend({model:t,rqlQuery:"exclude(changes)&limit(20)&sort(-d8OpenDate)"})});
+define(["../core/Collection","./D8Entry"],function(e,t){"use strict";return e.extend({model:t,rqlQuery:"exclude(changes)&limit(20)&sort(-crsRegisterDate)"})});
