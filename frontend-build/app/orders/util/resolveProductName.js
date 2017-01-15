12eecb294c420d2d7c136666cca9cc1a2729d3c4
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-01-15 17:28:34 +0100

    v0.103.0

---
+++ frontend-build/app/orders/util/resolveProductName.js
@@ -0,0 +1,3 @@
+// Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
+
+define(["underscore"],function(r){"use strict";var t=/^[A-Z0-9]{6}/;return function(i){if(!i)return"";var e=i.description||"",n=i.name||"";return r.isEmpty(e)?n.trim():r.isEmpty(n)?e.trim():t.test(n)?n.trim():(e||n).trim()}});
