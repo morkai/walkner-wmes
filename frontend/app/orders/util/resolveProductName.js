92e329e96c6b199089ae7ea3458b94faa7c8ed31
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-01-15 17:25:21 +0100

    Added a missing file.

---
+++ frontend/app/orders/util/resolveProductName.js
@@ -0,0 +1,39 @@
+// Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
+
+define([
+  'underscore'
+], function(
+  _
+) {
+  'use strict';
+
+  var PRODUCT_FAMILY_RE = /^[A-Z0-9]{6}/;
+
+  return function resolveProductName(orderData)
+  {
+    if (!orderData)
+    {
+      return '';
+    }
+
+    var description = orderData.description || '';
+    var name = orderData.name || '';
+
+    if (_.isEmpty(description))
+    {
+      return name.trim();
+    }
+
+    if (_.isEmpty(name))
+    {
+      return description.trim();
+    }
+
+    if (PRODUCT_FAMILY_RE.test(name))
+    {
+      return name.trim();
+    }
+
+    return (description || name).trim();
+  };
+});
