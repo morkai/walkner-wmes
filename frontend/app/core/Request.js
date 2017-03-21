892d899cb0cdbf48e7781f79820cb1c77b6c9e45
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-03-21 22:08:15 +0100

    app/core/Request: always define the `query` and `rql` getters.

--- frontend/app/core/Request.js
+++ frontend/app/core/Request.js
@@ -55,10 +55,7 @@ define([
      */
     this.rql = {};
 
-    if (this.queryString !== '')
-    {
-      this.defineGetters();
-    }
+    this.defineGetters();
   }
 
   Request.prototype.defineGetters = function()
