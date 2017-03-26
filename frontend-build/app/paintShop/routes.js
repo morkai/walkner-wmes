cf5876ce0172a6e7f4cd7e12af58c585e44a63d2
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-03-26 20:33:47 +0200

    v0.107.0

--- frontend-build/app/paintShop/routes.js
+++ frontend-build/app/paintShop/routes.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../router","../viewport","../user","./PaintShopOrderCollection","./pages/PaintShopPage","i18n!app/nls/paintShop"],function(e,n,o,r,t){"use strict";e.map("/paintShop",o.auth("LOCAL","PAINT_SHOP:VIEW"),function(e){n.showPage(new t({fullscreen:void 0!==e.query.fullscreen,model:{orders:r.forCurrentDate()}}))})});
+define(["../router","../viewport","../user","./PaintShopOrderCollection","./pages/PaintShopPage","i18n!app/nls/paintShop"],function(e,a,n,o,t){"use strict";e.map("/paintShop/:date",n.auth("LOCAL","PAINT_SHOP:VIEW"),function(e){a.showPage(new t({fullscreen:void 0!==e.query.fullscreen,model:{orders:o.forDate(e.params.date)}}))})});
