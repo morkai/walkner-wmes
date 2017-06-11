3399f55296bddb79dbadddb35277a4863fa6b256
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-06-11 23:50:07 +0200

    v0.113.0

--- frontend-build/app/core/util/uuid.js
+++ frontend-build/app/core/util/uuid.js
@@ -6,4 +6,4 @@
  * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
  **/
 
-define([],function(){"use strict";for(var r=[],t=0;256>t;++t)r[t]=(16>t?"0":"")+t.toString(16).toUpperCase();return function(){var t=4294967295*Math.random()|0,n=4294967295*Math.random()|0,a=4294967295*Math.random()|0,o=4294967295*Math.random()|0;return r[255&t]+r[t>>8&255]+r[t>>16&255]+r[t>>24&255]+"-"+r[255&n]+r[n>>8&255]+"-"+r[n>>16&15|64]+r[n>>24&255]+"-"+r[63&a|128]+r[a>>8&255]+"-"+r[a>>16&255]+r[a>>24&255]+r[255&o]+r[o>>8&255]+r[o>>16&255]+r[o>>24&255]}});
+define([],function(){"use strict";for(var r=[],t=0;t<256;++t)r[t]=(t<16?"0":"")+t.toString(16).toUpperCase();return function(){var t=4294967295*Math.random()|0,n=4294967295*Math.random()|0,a=4294967295*Math.random()|0,o=4294967295*Math.random()|0;return r[255&t]+r[t>>8&255]+r[t>>16&255]+r[t>>24&255]+"-"+r[255&n]+r[n>>8&255]+"-"+r[n>>16&15|64]+r[n>>24&255]+"-"+r[63&a|128]+r[a>>8&255]+"-"+r[a>>16&255]+r[a>>24&255]+r[255&o]+r[o>>8&255]+r[o>>16&255]+r[o>>24&255]}});
--- frontend-build/app/orders/Operation.js
+++ frontend-build/app/orders/Operation.js
@@ -1,3 +1,3 @@
 // Part of <http://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Model"],function(e){"use strict";var t=["machineSetupTime","machineTime","laborSetupTime","laborTime"];return e.extend({defaults:{no:null,workCenter:null,name:null,qty:null,unit:null,machineSetupTime:-1,machineTime:-1,laborSetupTime:-1,laborTime:-1},toJSON:function(){var n=e.prototype.toJSON.call(this);return n.qty&&(n.qtyUnit=n.qty,n.unit&&(n.qtyUnit+=" "+n.unit)),t.forEach(function(e){-1===n[e]&&(n[e]=null)}),n}})});
+define(["../core/Model"],function(e){"use strict";var t=["machineSetupTime","machineTime","laborSetupTime","laborTime"];return e.extend({defaults:{no:null,workCenter:null,name:null,qty:null,unit:null,machineSetupTime:-1,machineTime:-1,laborSetupTime:-1,laborTime:-1},toJSON:function(){var n=e.prototype.toJSON.call(this);return n.qty&&(n.qtyUnit=n.qty,n.unit&&(n.qtyUnit+=" "+n.unit)),t.forEach(function(e){n[e]===-1&&(n[e]=null)}),n}})});
