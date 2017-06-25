b8ae7db2afcef02e9857171158120daca925b691
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-06-26 01:30:04 +0200

    v0.114.1

--- frontend-build/app/subdivisions/views/SubdivisionDetailsView.js
+++ frontend-build/app/subdivisions/views/SubdivisionDetailsView.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/core/views/DetailsView","app/data/views/renderOrgUnitPath","app/subdivisions/templates/details"],function(e,i,t){"use strict";return e.extend({template:t,localTopics:{"divisions.synced":"render"},serializeDetails:function(e){return e.serialize({renderOrgUnitPath:i})}})});
+define(["app/core/views/DetailsView","app/orgUnits/util/renderOrgUnitPath","app/subdivisions/templates/details"],function(e,i,t){"use strict";return e.extend({template:t,localTopics:{"divisions.synced":"render"},serializeDetails:function(e){return e.serialize({renderOrgUnitPath:i})}})});
--- frontend-build/app/workCenters/views/WorkCenterDetailsView.js
+++ frontend-build/app/workCenters/views/WorkCenterDetailsView.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/data/views/renderOrgUnitPath","app/core/views/DetailsView","app/workCenters/templates/details"],function(e,r,i){"use strict";return r.extend({template:i,localTopics:{"divisions.synced":"render","subdivisions.synced":"render","mrpControllers.synced":"render","prodFlows.synced":"render"},serialize:function(){var i=r.prototype.serialize.call(this);return i.orgUnitPath=e(this.model,!0),i}})});
+define(["app/orgUnits/util/renderOrgUnitPath","app/core/views/DetailsView","app/workCenters/templates/details"],function(e,r,i){"use strict";return r.extend({template:i,localTopics:{"divisions.synced":"render","subdivisions.synced":"render","mrpControllers.synced":"render","prodFlows.synced":"render"},serialize:function(){var i=r.prototype.serialize.call(this);return i.orgUnitPath=e(this.model,!0),i}})});
