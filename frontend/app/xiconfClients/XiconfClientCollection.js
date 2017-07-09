98a033e40fd2cf38d4262c81aa36d2969e9840b7
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-07-09 15:05:36 +0200

    xiconfClients: sort by prod line naturally.

--- frontend/app/xiconfClients/XiconfClientCollection.js
+++ frontend/app/xiconfClients/XiconfClientCollection.js
@@ -28,6 +28,30 @@ define([
       });
     },
 
+    comparator: function(a, b)
+    {
+      a = a.get('prodLine') || '';
+      b = b.get('prodLine') || '';
+
+      if (a === b)
+      {
+        return 0;
+      }
+
+      var aMatches = a.match(/^(.*?)([0-9]+)?$/);
+      var bMatches = b.match(/^(.*?)([0-9]+)?$/);
+
+      if (aMatches[1] !== bMatches[1])
+      {
+        return aMatches[1].localeCompare(bMatches[1]);
+      }
+
+      var aN = +(aMatches[2] || 0);
+      var bN = +(bMatches[2] || 0);
+
+      return aN < bN ? -1 : 1;
+    },
+
     getUsedLicenseIds: function()
     {
       var licenseIds = {};
