067e7174b3cd79af530a8fc74e9e2bbf5465478b
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-07-09 13:52:55 +0200

    xiconfOrders: list: limit the results to the last week by default.

--- frontend/app/xiconfOrders/XiconfOrderCollection.js
+++ frontend/app/xiconfOrders/XiconfOrderCollection.js
@@ -17,6 +17,8 @@ define([
 
     rqlQuery: function(rql)
     {
+      var today = time.getMoment().startOf('day');
+
       return rql.Query.fromObject({
         fields: {
           items: 0
@@ -28,7 +30,8 @@ define([
         selector: {
           name: 'and',
           args: [
-            {name: 'lt', args: ['reqDate', time.getMoment().startOf('day').add(1, 'days').valueOf()]}
+            {name: 'ge', args: ['reqDate', today.subtract(7, 'days').valueOf()]},
+            {name: 'lt', args: ['reqDate', today.add(8, 'days').valueOf()]}
           ]
         }
       });
