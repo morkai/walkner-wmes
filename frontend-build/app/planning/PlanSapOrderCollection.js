024540297cbce026b36980bb250e0b4badb269a1
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-11-08 01:55:15 +0100

    Updated the frontend build.

--- frontend-build/app/planning/PlanSapOrderCollection.js
+++ frontend-build/app/planning/PlanSapOrderCollection.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Collection","./PlanSapOrder"],function(n,i){"use strict";return n.extend({model:i,initialize:function(n,i){this.plan=i&&i.plan},url:function(){return"/planning/sapOrders/"+this.plan.id}})});
+define(["../core/Collection","./PlanSapOrder"],function(n,i){"use strict";return n.extend({model:i,initialize:function(n,i){this.plan=i&&i.plan,this.mrp=i&&i.mrp},url:function(){var n="/planning/sapOrders/"+this.plan.id;return this.mrp&&(n+="?mrp="+this.mrp),n}})});
