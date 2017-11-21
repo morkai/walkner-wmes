fb8cba75e12ccc87056c72ce522082ed621abb8a
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-11-21 23:46:10 +0100

    Updated the frontend build.

--- frontend-build/app/core/util/matchesEquals.js
+++ frontend-build/app/core/util/matchesEquals.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["underscore"],function(r){"use strict";return function(n,e,t){var u=r.find(n.selector.args,function(r){return"eq"===r.name&&r.args[0]===e});return!u||u.args[1]===t}});
+define(["underscore"],function(r){"use strict";return function(n,e,i){if(void 0===i)return!0;var t=r.find(n.selector.args,function(r){return("eq"===r.name||"in"===r.name)&&r.args[0]===e});return!t||("eq"===t.args[0]?t.args[1]===i:t.args[1].indexOf(i)!==-1)}});
