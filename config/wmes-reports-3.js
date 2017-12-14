fa047b71d1b275d54fc318780ad0240cb29b15d6
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2017-12-14 19:04:43 +0100

    Added a new process config: wmes-reports-3.

---
+++ config/wmes-reports-3.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const ports = require('./wmes-ports');
+const config = module.exports = require('./wmes-reports-1');
+
+config.id = 'wmes-reports-3';
+
+Object.assign(config['messenger/server'], ports[config.id]);
