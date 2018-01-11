6bd05487a26473f73f34b93b1c0644052228da74
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-01-11 16:46:48 +0100

    Updated the frontend build.

--- frontend-build/app/paintShopPaints/pages/PaintShopPaintListPage.js
+++ frontend-build/app/paintShopPaints/pages/PaintShopPaintListPage.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/core/pages/ListPage","i18n!app/nls/paintShopPaints"],function(i){"use strict";return i.extend({baseBreadcrumb:!0,columns:[{id:"_id",className:"is-min"},{id:"shelf",className:"is-min"},{id:"bin",className:"is-min"},"name"]})});
+define(["app/core/util/pageActions","app/core/pages/ListPage","i18n!app/nls/paintShopPaints"],function(i,s){"use strict";return s.extend({baseBreadcrumb:!0,actions:function(){return[i.jump(this,this.collection,{mode:"id"}),i.add(this.collection,"PAINT_SHOP:MANAGE")]},columns:[{id:"_id",className:"is-min"},{id:"shelf",className:"is-min"},{id:"bin",className:"is-min"},"name"]})});
