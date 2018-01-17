09a05beab5599057a7c2ec4a9a7c070a939daee1
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-01-17 15:16:35 +0100

    paintShop: paints: show a custom error if a paint with the specified 12NC already exists.

--- frontend/app/paintShopPaints/views/PaintShopPaintFormView.js
+++ frontend/app/paintShopPaints/views/PaintShopPaintFormView.js
@@ -1,9 +1,13 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
 define([
+  'underscore',
+  'app/i18n',
   'app/core/views/FormView',
   'app/paintShopPaints/templates/form'
 ], function(
+  _,
+  t,
   FormView,
   template
 ) {
@@ -12,6 +16,35 @@ define([
   return FormView.extend({
 
     template: template,
+
+    events: _.assign({
+
+      'input #-_id': function(e)
+      {
+        var view = this;
+        var nc12 = view.$id('_id').val();
+
+        if (nc12.length !== 12)
+        {
+          e.target.setCustomValidity('');
+
+          return;
+        }
+
+        var req = this.ajax({method: 'HEAD', url: '/paintShop/paints/' + nc12});
+
+        req.fail(function()
+        {
+          e.target.setCustomValidity('');
+        });
+
+        req.done(function()
+        {
+          e.target.setCustomValidity(req.status === 200 ? t('paintShopPaints', 'FORM:ERROR:alreadyExists') : '');
+        });
+      }
+
+    }, FormView.prototype.events),
 
     afterRender: function()
     {
