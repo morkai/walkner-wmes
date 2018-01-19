11993b891b66f514de6a41992146e616b3070335
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-01-19 12:03:27 +0100

    Updated the frontend build.

--- frontend-build/app/paintShopPaints/PaintShopPaint.js
+++ frontend-build/app/paintShopPaints/PaintShopPaint.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Model"],function(i){"use strict";return i.extend({urlRoot:"/paintShop/paints",clientUrlRoot:"#paintShop/paints",privilegePrefix:"PAINT_SHOP",nlsDomain:"paintShopPaints"})});
+define(["../core/Model"],function(i){"use strict";return i.extend({urlRoot:"/paintShop/paints",clientUrlRoot:"#paintShop/paints",topicPrefix:"paintShop.paints",privilegePrefix:"PAINT_SHOP",nlsDomain:"paintShopPaints"})});
