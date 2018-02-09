eed6c3194158783740e476c326186895b39105a9
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-02-09 12:37:06 +0100

    Updated the frontend build.

--- frontend-build/app/fte/util/fractions.js
+++ frontend-build/app/fte/util/fractions.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define([],function(){"use strict";var r="function"==typeof Number.prototype.toLocaleString?"toLocaleString":"toString";return{parse:function(r,t){if(!r)return 0;var e=parseFloat(r.replace(",",".").replace(/[^0-9.\-]+/g,""));return isNaN(e)||!t&&e<0?0:e},round:function(t){if(!t)return"0";var e=(Math.round(1e4*t)/1e4)[r]();return/[.,](999|001)$/.test(e)?Math.round(t)[r]():e}}});
+define([],function(){"use strict";var r="function"==typeof Number.prototype.toLocaleString?"toLocaleString":"toString";return{parse:function(r,t){if(!r)return 0;var e=parseFloat(r.replace(",",".").replace(/[^0-9.\-]+/g,""));return isNaN(e)||!t&&e<0?0:e},round:function(t){if(!t||t<0)return"0";var e=(Math.round(1e4*t)/1e4)[r]();return/[.,](999|001)$/.test(e)?Math.round(t)[r]():e}}});
