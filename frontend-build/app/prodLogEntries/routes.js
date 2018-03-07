04f3c0e40d7a7c55524f0775944099e90b089f1e
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-03-08 00:00:16 +0100

    Updated the frontend build.

--- frontend-build/app/prodLogEntries/routes.js
+++ frontend-build/app/prodLogEntries/routes.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../router","../viewport","../user"],function(r,n,p){"use strict";var o=p.auth("LOCAL","PROD_DATA:VIEW");r.map("/prodLogEntries",o,function(r){n.loadPage(["app/prodLogEntries/pages/ProdLogEntryListPage","i18n!app/nls/prodLogEntries","i18n!app/nls/prodShifts","i18n!app/nls/prodShiftOrders","i18n!app/nls/prodDowntimes"],function(n){return new n({rql:r.rql})})})});
+define(["../router","../viewport","../user"],function(r,n,p){"use strict";var o=p.auth("PROD_DATA:VIEW");r.map("/prodLogEntries",o,function(r){n.loadPage(["app/prodLogEntries/pages/ProdLogEntryListPage","i18n!app/nls/prodLogEntries","i18n!app/nls/prodShifts","i18n!app/nls/prodShiftOrders","i18n!app/nls/prodDowntimes"],function(n){return new n({rql:r.rql})})})});
