ce045b33a79404a7e03591c00c44c7c2faec3608
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-03-25 23:38:50 +0200

    Updated the frontend build.

--- frontend-build/app/planning/PlanningSetting.js
+++ frontend-build/app/planning/PlanningSetting.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../settings/Setting"],function(t){"use strict";return t.extend({urlRoot:"/planning/settings"})});
+define(["../settings/Setting"],function(n){"use strict";return n.extend({nlsDomain:"planning",urlRoot:"/planning/settings"})});
