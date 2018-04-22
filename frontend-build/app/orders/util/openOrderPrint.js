e7f5c457002319157d194f6a50651b638d9aea9f
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-04-22 22:56:06 +0200

    Updated the frontend build.

--- frontend-build/app/orders/util/openOrderPrint.js
+++ frontend-build/app/orders/util/openOrderPrint.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["jquery","app/core/util/html2pdf"],function(t,n){"use strict";return function(e){t.ajax({url:"/orders/"+e.join("+")+".html",dataType:"html"}).done(function(t){n(t)})}});
+define(["jquery","app/core/util/html2pdf"],function(t,n){"use strict";return function(e,r){t.ajax({url:"/orders/"+e.join("+")+".html",dataType:"html"}).done(function(t){n(t,r)})}});
