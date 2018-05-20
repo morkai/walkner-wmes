9876274687e48e0ebacb930d765998b1164797e7
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-05-20 15:05:55 +0200

    Updated the frontend build.

--- frontend-build/app/planning/WhOrderStatus.js
+++ frontend-build/app/planning/WhOrderStatus.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Model"],function(t){"use strict";var e={status:0,updatedAt:new Date,updater:null};return t.extend({urlRoot:"/planning/whOrderStatuses",topicPrefix:"planning.whOrderStatuses",privilegePrefix:"PLANNING",nlsDomain:"planning",defaults:function(){return{orders:{}}},setOrderStatus:function(t,e){this.attributes.orders[t]=e,this.trigger("change:orders",t,e)},getOrderStatus:function(t){return this.attributes.orders[t]||e}})});
+define(["../i18n","../core/Model"],function(t,e){"use strict";return e.extend({urlRoot:"/planning/whOrderStatuses",topicPrefix:"planning.whOrderStatuses",privilegePrefix:"PLANNING",nlsDomain:"planning",serialize:function(){return{status:this.get("status"),label:t("planning","wh:status:"+this.get("status"),{qtySent:this.get("qtySent")})}}})});
