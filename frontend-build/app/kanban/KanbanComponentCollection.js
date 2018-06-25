60bb36b69d45d015f083ffc90634f547b903484d
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-06-25 12:21:02 +0200

    Updated the frontend build.

--- frontend-build/app/kanban/KanbanComponentCollection.js
+++
@@ -1,3 +0,0 @@
-// Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
-
-define(["../core/Collection","./KanbanComponent"],function(n,e){"use strict";return n.extend({model:e,paginate:!1,setUpPubsub:function(n){}})});
