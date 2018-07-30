278364335dc1574694c853d56da430e91789f139
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-07-30 02:23:31 +0200

    Updated the frontend build.

---
+++ frontend-build/app/wh/WhUser.js
@@ -0,0 +1,3 @@
+// Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
+
+define(["../i18n","../core/Model"],function(e,i){"use strict";return i.extend({urlRoot:"/wh/users",topicPrefix:"wh.users",privilegePrefix:"WH",nlsDomain:"wh",labelAttribute:"label"})});
