e97ff416d86883775b1f34f19a0166b887ef319e
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-08-09 12:25:46 +0200

    Updated the frontend build.

--- frontend-build/app/kanban/KanbanSettingCollection.js
+++ frontend-build/app/kanban/KanbanSettingCollection.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["underscore","../settings/SettingCollection","./KanbanSetting"],function(e,n,t){"use strict";return n.extend({model:t,topicSuffix:"kanban.**",getValue:function(e){var n=this.get("kanban."+e);return n?n.getValue():null},prepareValue:function(e,n){}})});
+define(["underscore","../settings/SettingCollection","./KanbanSetting"],function(n,e,t){"use strict";return e.extend({model:t,topicSuffix:"kanban.**",getValue:function(n){var e=this.get("kanban."+n);return e?e.getValue():null}})});
