6318f894408a9862c2afb228d4db953434abfd29
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-10-04 23:49:52 +0200

    Updated the frontend build.

--- frontend-build/app/qiKinds/QiKind.js
+++ frontend-build/app/qiKinds/QiKind.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../i18n","../core/Model","../data/orgUnits"],function(i,n,e){"use strict";return n.extend({urlRoot:"/qi/kinds",clientUrlRoot:"#qi/kinds",topicPrefix:"qi.kinds",privilegePrefix:"QI:DICTIONARIES",nlsDomain:"qiKinds",labelAttribute:"name",serialize:function(){var n=this.toJSON(),o=e.getByTypeAndId("division",n.division);return n.division=n.division?o?o.getLabel():n.division:i.bound("qiKinds","ordersDivision"),n}})});
+define(["../i18n","../core/Model","../data/orgUnits"],function(i,e,n){"use strict";return e.extend({urlRoot:"/qi/kinds",clientUrlRoot:"#qi/kinds",topicPrefix:"qi.kinds",privilegePrefix:"QI:DICTIONARIES",nlsDomain:"qiKinds",labelAttribute:"name",serialize:function(){var e=this.toJSON(),o=n.getByTypeAndId("division",e.division);return e.division=e.division?o?o.getLabel():e.division:i.bound("qiKinds","ordersDivision"),e.order=i("core","BOOL:"+e.order),e}})});
