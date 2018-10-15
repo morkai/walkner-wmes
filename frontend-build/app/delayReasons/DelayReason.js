a2f05606c9c830890a0bfe87e7756ccbcef57969
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-10-15 23:41:39 +0200

    Updated the frontend build.

--- frontend-build/app/delayReasons/DelayReason.js
+++ frontend-build/app/delayReasons/DelayReason.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Model"],function(e){"use strict";return e.extend({urlRoot:"/delayReasons",clientUrlRoot:"#delayReasons",topicPrefix:"delayReasons",privilegePrefix:"DICTIONARIES",nlsDomain:"delayReasons",labelAttribute:"name",defaults:{name:null}})});
+define(["../i18n","../core/Model"],function(e,t){"use strict";return t.extend({urlRoot:"/delayReasons",clientUrlRoot:"#delayReasons",topicPrefix:"delayReasons",privilegePrefix:"DICTIONARIES",nlsDomain:"delayReasons",labelAttribute:"name",defaults:{active:!0},serializeRow:function(){var t=this.toJSON();return t.active=e("core","BOOL:"+t.active),Object.keys(t.drm||{}).forEach(function(e){t[e]=t.drm[e]||""}),t}})});
--- frontend-build/app/delayReasons/DelayReasonCollection.js
+++ frontend-build/app/delayReasons/DelayReasonCollection.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["../core/Collection","./DelayReason"],function(e,n){"use strict";return e.extend({model:n,rqlQuery:"select(name)&sort(name)",comparator:"name"})});
+define(["../core/Collection","./DelayReason"],function(e,n){"use strict";return e.extend({model:n,rqlQuery:"sort(name)",comparator:"name"})});
