d65214dfc25b6f28818e0947a2ec76b0ad0f07f2
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-10-17 22:57:22 +0200

    Updated the frontend build.

--- frontend-build/app/orderBomMatchers/views/OrderBomMatcherListView.js
+++ frontend-build/app/orderBomMatchers/views/OrderBomMatcherListView.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["app/core/views/ListView"],function(e){"use strict";return e.extend({className:"is-clickable",columns:["description",{id:"active",className:"is-min"},"mrp","nc12","name","components"]})});
+define(["app/core/views/ListView"],function(e){"use strict";return e.extend({className:"is-clickable",columns:["description",{id:"active",className:"is-min"},"line","mrp","nc12","name","components"]})});
