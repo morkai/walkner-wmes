14b6c0679b84dbea7884f7071aa5ed382485fdc0
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-10-23 13:55:38 +0200

    Updated the frontend build.

--- frontend-build/app/delayReasons/views/DelayReasonFormView.js
+++ frontend-build/app/delayReasons/views/DelayReasonFormView.js
@@ -1,3 +1,3 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
-define(["underscore","app/core/views/FormView","app/delayReasons/templates/form"],function(e,r,t){"use strict";return r.extend({template:t,serializeForm:function(r){return r.drm=e.defaults({},r.drm,{man:"",machine:"",method:"",material:""}),r}})});
+define(["app/core/views/FormView","app/delayReasons/templates/form"],function(e,t){"use strict";return e.extend({template:t})});
