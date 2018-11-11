f99d6b2fa48e8a9c1dc52dbf67200985a002e6bd
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-11-11 20:10:39 +0100

    orgUnits: re-enabled the recent line name changes.

--- backend/modules/orgUnits/fix.js
+++ backend/modules/orgUnits/fix.js
@@ -14,11 +14,9 @@ module.exports = function setUpOrgUnitsFix(app, module)
     'WB-4': 'WB-4',
     'WU-1': 'WU-1',
     'WU-2': 'WU-2',
-    /*
     'AV1': 'LM-37',
     'AV2': 'LM-38',
     'OFC': 'LM-39'
-    */
   };
   const prodLineToOrgUnits = new Map();
   const prodLineToPlainOrgUnits = new Map();
