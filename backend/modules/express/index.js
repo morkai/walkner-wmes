91dd32a1bf56d57a269098ac907cfe696c4cc272
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-11-14 13:00:53 +0100

    frontend: moved `favicon.ico` to the root static directory.

--- backend/modules/express/index.js
+++ backend/modules/express/index.js
@@ -110,7 +110,7 @@ exports.start = function startExpressModule(app, expressModule, done)
 
   expressApp.use(function(req, res, next)
   {
-    req.isFrontendRequest = /^\/(app|assets|files|vendor)/.test(req.url) || /^[a-z\-]+\.js$/.test(req.url);
+    req.isFrontendRequest = /^\/(app|assets|files|vendor|favicon)/.test(req.url) || /^[a-z\-]+\.js$/.test(req.url);
 
     next();
   });
