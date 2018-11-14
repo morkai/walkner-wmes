df24bf673fb4d14611e19b02bb8d59278db065d0
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-11-14 15:18:54 +0100

    tools: redbird: allow specifying a custom config file.

--- bin/redbird.js
+++ bin/redbird.js
@@ -1,25 +1,3 @@
 'use strict';
 
-require('redbird')({
-  port: 80,
-  ssl: {
-    port: 443,
-    key: `${__dirname}/../config/production/ssl/privkey.pem`,
-    cert: `${__dirname}/../config/production/ssl/cert.pem`,
-    http2: true
-  },
-  bunyan: {
-    name: 'wmes'
-  },
-  resolvers: [
-    function(host, url, req)
-    {
-      if (/^\/(app|assets|files|vendor|favicon)/.test(req.url) || /^[a-z\-]+\.js$/.test(req.url))
-      {
-        return 'http://127.0.0.1:10081';
-      }
-
-      return 'http://127.0.0.1:10080';
-    }
-  ]
-});
+require('redbird')(require(process.argv[2]));
