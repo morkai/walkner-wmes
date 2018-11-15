247794796842689adbf7d0da352091873002e85b
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-11-15 03:40:35 +0100

    events: changed `minutesForSafety.deleted` to `minutesForSafetyCards.deleted`.

--- wmes-update.js
+++ wmes-update.js
@@ -3,14 +3,4 @@
 
 'use strict';
 
-db.orders.find({startDate: {$gte: new Date("2018-11-01T00:00:00")}, psStatus: {$ne: null}}, {changes: 1}).forEach(o =>
-{
-  const changes = o.changes.filter(
-    c => !c.user || !(c.user.label === 'System' && c.source === 'ps' && Object.keys(c.oldValues).length === 0)
-  );
-
-  if (changes.length !== o.changes.length)
-  {
-    db.orders.update({_id: o._id}, {$set: {changes: changes}});
-  }
-});
+db.events.updateMany({type: 'minutesForSafety.deleted'}, {$set: {type: 'minutesForSafetyCards.deleted'}});
