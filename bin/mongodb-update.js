395bfe34fb8a53d434b7da371775d7f19b9af122
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2018-12-01 00:02:22 +0100

    orderDocuments: record all changes to the document files.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -5,40 +5,21 @@
 
 load('./mongodb-helpers.js');
 
-const oldId = 'IR3III';
-const newId = 'SM22';
+const time = new Date();
+const user = {
+  id: null,
+  label: 'System'
+};
 
-db.plans.find({_id: {$gte: new Date('2018-08-07T00:00:00.000Z')}, 'lines._id': 'IR3III'}).forEach(plan =>
+db.orderdocumentchanges.deleteMany({});
+
+db.orderdocumentfiles.find({}).forEach(f =>
 {
-  var settings = db.plansettings.findOne({_id: plan._id});
-
-  plan.lines.forEach(line =>
-  {
-    if (line._id === oldId)
-    {
-      line._id = newId;
-    }
+  db.orderdocumentchanges.insertOne({
+    time,
+    user,
+    type: 'fileAdded',
+    nc15: f._id,
+    data: f
   });
-
-  settings.lines.forEach(line =>
-  {
-    if (line._id === oldId)
-    {
-      line._id = newId;
-    }
-  });
-
-  settings.mrps.forEach(mrp =>
-  {
-    mrp.lines.forEach(line =>
-    {
-      if (line._id === oldId)
-      {
-        line._id = newId;
-      }
-    });
-  });
-
-  db.plans.updateOne({_id: plan._id}, {$set: {lines: plan.lines}});
-  db.plansettings.updateOne({_id: plan._id}, {$set: {lines: settings.lines, mrps: settings.mrps}});
 });
