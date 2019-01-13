e373942827597be2e3a3964da28f67d325bd3292
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-01-13 16:26:45 +0100

    wmes-fap: removed the testing warning from the Add form.

--- frontend/app/wmes-fap-entries/views/AddFormView.js
+++ frontend/app/wmes-fap-entries/views/AddFormView.js
@@ -37,25 +37,6 @@ define([
 
     events: _.assign({
 
-      'mousedown': function(e)
-      {
-        if (!this.$(e.target).closest('.fap-addForm-warning').length)
-        {
-          this.hideWarning();
-        }
-      },
-      'click #-submit': function()
-      {
-        this.showWarning();
-      },
-      'click #-reject': function()
-      {
-        this.hideWarning();
-      },
-      'select2-focus': function()
-      {
-        this.hideWarning();
-      },
       'click #-cancel': function()
       {
         this.trigger('cancel');
@@ -198,16 +179,6 @@ define([
       view.setUpDnd($backdrop);
       view.updateNotifications();
       view.focusInput();
-    },
-
-    showWarning: function()
-    {
-      this.$id('warning').removeClass('hidden');
-    },
-
-    hideWarning: function()
-    {
-      this.$id('warning').addClass('hidden');
     },
 
     renderUploads: function()
