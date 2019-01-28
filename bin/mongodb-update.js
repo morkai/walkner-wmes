a7f34dca61582cbf1f74e56c5272b2ccf7e3242d
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-01-28 21:29:56 +0100

    Drop unused/invalid collections.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -5,38 +5,6 @@
 
 load('./mongodb-helpers.js');
 
-db.fapentries.find({analysisNeed: true}, {changes: 1}).forEach(entry =>
-{
-  let analysisStartedAt = null;
-  let analysisFinishedAt = null;
-
-  entry.changes.forEach(change =>
-  {
-    if (change.data.analysisNeed)
-    {
-      if (change.data.analysisNeed[1])
-      {
-        analysisStartedAt = change.date;
-      }
-      else
-      {
-        analysisStartedAt = null;
-        analysisFinishedAt = null;
-      }
-    }
-
-    if (change.data.analysisDone)
-    {
-      if (change.data.analysisDone[1])
-      {
-        analysisFinishedAt = change.date;
-      }
-      else
-      {
-        analysisFinishedAt = null;
-      }
-    }
-  });
-
-  db.fapentries.updateOne({_id: entry._id}, {$set: {analysisStartedAt, analysisFinishedAt}});
-});
+db.prododwntimes.drop();
+db.icporesults.drop();
+db.hefflinestates.drop();
