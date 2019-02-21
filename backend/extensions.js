6edbaa73d5a82e1786da7841534fc1fe8f818fbe
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-02-21 18:05:47 +0100

    backend: fixed error pretty printing splitting messages containing `at` string instead of `  at `.

--- backend/extensions.js
+++ backend/extensions.js
@@ -83,7 +83,7 @@ if (process.env.NODE_ENV === 'development')
     }
     else if (errorStack)
     {
-      const matches = errorStack.match(/^(?:Error:\s*)?(.*?)(at.*?)?$/s);
+      const matches = errorStack.match(/^(?:Error:\s*)?(.*?)(\s{2,}at .*?)?$/s);
 
       if (matches)
       {
