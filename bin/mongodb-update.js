b543afe1ff673513ad306d697f9762fefa0a1ae5
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-03-10 17:40:24 +0100

    orders/importer/bom: store a map of component 12NC to name in a separate collection.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -3,4 +3,45 @@
 
 'use strict';
 
-db.paintshoporders.updateMany({qtyDlv: {$exists: false}}, {$set: {qtyDlv: 0}});
+var map = {};
+
+print('Fetching components...');
+
+db.orders.find({}, {'bom.nc12': 1, 'bom.name': 1}).forEach(o =>
+{
+  if (!Array.isArray(o.bom))
+  {
+    return;
+  }
+
+  o.bom.forEach(c =>
+  {
+    if (c.nc12.length !== 0)
+    {
+      map[c.nc12] = c.name;
+    }
+  });
+});
+
+var list = [];
+
+Object.keys(map).forEach(nc12 =>
+{
+  list.push({
+    _id: nc12,
+    name: map[nc12]
+  });
+});
+
+print('Removing components...');
+
+db.components.deleteMany({});
+
+print('Inserting components...');
+
+while (list.length)
+{
+  var chunk = list.splice(0, 100);
+
+  db.components.insertMany(chunk);
+}
