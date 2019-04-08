18f6026d4ef7b06b602b80be40e85f6c200dd80a
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-04-08 18:04:28 +0200

    prodShiftOrders: fixed invalid breadcrumbs for the Edit form.

--- frontend/app/prodShiftOrders/pages/EditProdShiftOrderFormPage.js
+++ frontend/app/prodShiftOrders/pages/EditProdShiftOrderFormPage.js
@@ -1,13 +1,9 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
 define([
-  'app/i18n',
-  'app/time',
   'app/core/pages/EditFormPage',
   '../views/ProdShiftOrderFormView'
 ], function(
-  t,
-  time,
   EditFormPage,
   ProdShiftOrderFormView
 ) {
@@ -15,22 +11,7 @@ define([
 
   return EditFormPage.extend({
 
-    FormView: ProdShiftOrderFormView,
-
-    breadcrumbs: function()
-    {
-      return [
-        {
-          label: t.bound('prodShiftOrders', 'BREADCRUMBS:browse'),
-          href: this.model.genClientUrl('base')
-        },
-        {
-          label: this.model.getLabel(),
-          href: this.model.genClientUrl()
-        },
-        t.bound('prodShiftOrders', 'BREADCRUMBS:editForm')
-      ];
-    }
+    FormView: ProdShiftOrderFormView
 
   });
 });
