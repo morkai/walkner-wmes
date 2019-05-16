3be485e486ec72763f089e8c3b758ff56c28d72b
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-05-17 00:29:38 +0200

    Updated the frontend build.

--- frontend-build/app/wmes-snf-tests/TestCollection.js
+++ frontend-build/app/wmes-snf-tests/TestCollection.js
@@ -1,1 +1,1 @@
-define(["../core/Collection","./Test"],function(e,t){"use strict";return e.extend({model:t,rqlQuery:"sort(-startedAt)&limit(-1337)"})});
+define(["../time","../core/Collection","./Test"],function(e,t,r){"use strict";return t.extend({model:r,rqlQuery:function(t){return t.Query.fromObject({fields:{startedAt:1,finishedAt:1,"program.name":1,result:1,orderNo:1,serialNo:1},sort:{startedAt:-1},limit:-1337,selector:{name:"and",args:[{name:"ge",args:["startedAt",e.getMoment().startOf("week").subtract(2,"weeks")]}]}})}})});
