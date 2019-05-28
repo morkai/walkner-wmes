2dd629b9000c7fd7f49be766f311545ff7a42504
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-05-28 13:19:08 +0200

    fap: fixed possible NULL-reference errors while resolving participants and duplicate users due to user ID type mismatch.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -3,20 +3,4 @@
 
 'use strict';
 
-db.trwbases.find({}).forEach(base =>
-{
-  base.clusters.forEach(cluster =>
-  {
-    if (!cluster.connector)
-    {
-      cluster.connector = 'na';
-    }
-
-    if (!cluster.image)
-    {
-      cluster.image = '';
-    }
-  });
-
-  db.trwbases.replaceOne({_id: base._id}, base);
-});
+db.users.updateMany({prodFunction: {$exists: false}}, {$set: {prodFunction: null}});
