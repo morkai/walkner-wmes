a37a7396072e8bf1a1a6abaf13e2239221b3c24b
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-06-01 00:50:33 +0200

    wh: added a new property to orders: psStatus.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -14,3 +14,21 @@ db.kanbancomponents.find({}, {storageBin: 1}).forEach(component =>
     db.kanbancomponents.deleteOne({_id: component._id});
   }
 });
+
+db.whorders.dropIndex('order_1_date_-1');
+
+db.whorders.deleteMany({date: {$lt: new Date('2019-02-21T00:00:00.000Z')}});
+
+db.whorders.find({psStatus: {$exists: false}}, {order: 1}).forEach(whOrder =>
+{
+  var sapOrder = db.orders.findOne({_id: whOrder.order});
+
+  db.whorders.updateOne({_id: whOrder._id}, {
+    $set: {
+      psStatus: !sapOrder ? 'unknown' : sapOrder.psStatus
+    },
+    $unset: {
+      painted: 1
+    }
+  });
+});
