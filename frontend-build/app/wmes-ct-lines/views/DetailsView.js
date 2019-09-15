f4730b47f97c37097fa12df45a7cdffe2e829f0d
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-09-15 19:13:04 +0200

    Updated the frontend build.

---
+++ frontend-build/app/wmes-ct-lines/views/DetailsView.js
@@ -0,0 +1,1 @@
+define(["app/core/views/DetailsView","app/wmes-ct-lines/templates/details"],function(e,t){"use strict";return e.extend({template:t,getTemplateData:function(){return{type:this.model.get("type")}}})});
