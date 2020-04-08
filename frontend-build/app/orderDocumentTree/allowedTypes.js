2ee9d36f629dd35b53015306dc9b5142f087057c
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-04-09 00:41:36 +0200

    Updated the frontend build.

---
+++ frontend-build/app/orderDocumentTree/allowedTypes.js
@@ -0,0 +1,1 @@
+define([],function(){"use strict";return{"application/pdf":"pdf","text/plain":"txt","application/json":"json","application/vnd.openxmlformats-officedocument.wordprocessingml.document":"docx","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":"xlsx","application/vnd.openxmlformats-officedocument.presentationml.presentation":"pptx","video/mp4":"mp4"}});
