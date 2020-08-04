3a41e9b8492aca5de2614d97a12f72290aa77893
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2020-08-04 20:57:09 +0200

    dummyPaint: paints: fixed the mass update dialog.

--- frontend/app/wmes-dummyPaint-paints/views/MassUpdateView.js
+++ frontend/app/wmes-dummyPaint-paints/views/MassUpdateView.js
@@ -1,9 +1,11 @@
 // Part of <https://miracle.systems/p/walkner-wmes> licensed under <CC BY-NC-SA 4.0>
 
 define([
+  'app/viewport',
   'app/core/views/FormView',
   'app/wmes-dummyPaint-paints/templates/massUpdate'
 ], function(
+  viewport,
   FormView,
   template
 ) {
@@ -13,6 +15,15 @@ define([
 
     template: template,
 
+    nlsDomain: 'wmes-dummyPaint-paints',
+
+    events: Object.assign({
+
+      'change #-code': 'checkRequiredFields',
+      'change #-old12': 'checkRequiredFields'
+
+    }, FormView.prototype.events),
+
     request: function(formData)
     {
       return this.ajax({
@@ -20,6 +31,31 @@ define([
         url: '/dummyPaint/paints;massUpdate',
         data: JSON.stringify(formData)
       });
+    },
+
+    afterRender: function()
+    {
+      FormView.prototype.afterRender.apply(this, arguments);
+
+      this.checkRequiredFields();
+    },
+
+    checkRequiredFields: function()
+    {
+      var $code = this.$id('code');
+      var $old12 = this.$id('old12');
+
+      $code[0].setCustomValidity($code.val().length || $old12.val().length ? '' : this.t('massUpdate:required'));
+    },
+
+    getFailureText: function()
+    {
+      return this.t('massUpdate:failure');
+    },
+
+    handleSuccess: function()
+    {
+      viewport.closeDialog();
     }
 
   });
