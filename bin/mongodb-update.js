d352f01f4c6d984055841bf82c9ca982593e46de
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2020-08-06 21:32:14 +0200

    wh v2: insert the default redir reasons.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -64,3 +64,65 @@ db.orders.updateMany(
   {scheduledStartDate: {$gt: new Date('2020-04-08T00:00:00Z')}, drillStatus: {$exists: false}},
   {$set: {drillStatus: 'unknown'}}
 );
+
+try
+{
+  db.oldwhredirreasons.insertMany([
+    /* 1 createdAt:2020-08-04T21:00:43.000Z*/
+    {
+      "_id" : ObjectId("5f29cc7bb2ccd433a0703896"),
+      "active" : true,
+      "label" : "Złe rozplanowanie (błąd ludzki)"
+    },
+
+    /* 2 createdAt:2020-08-04T21:00:31.000Z*/
+    {
+      "_id" : ObjectId("5f29cc6fb2ccd433a0703893"),
+      "active" : true,
+      "label" : "Zlecenie przekierowania osób trzecich"
+    },
+
+    /* 3 createdAt:2020-08-04T21:00:25.000Z*/
+    {
+      "_id" : ObjectId("5f29cc69b2ccd433a0703890"),
+      "active" : true,
+      "label" : "Wydajność"
+    },
+
+    /* 4 createdAt:2020-08-04T21:00:17.000Z*/
+    {
+      "_id" : ObjectId("5f29cc61b2ccd433a070388d"),
+      "active" : true,
+      "label" : "Puste pole 10 minut przy linii w ciągu zmiany"
+    },
+
+    /* 5 createdAt:2020-08-04T21:00:10.000Z*/
+    {
+      "_id" : ObjectId("5f29cc5ab2ccd433a070388a"),
+      "active" : true,
+      "label" : "Puste pole 10 minut przy linii na rano"
+    },
+
+    /* 6 createdAt:2020-08-04T20:59:58.000Z*/
+    {
+      "_id" : ObjectId("5f29cc4eb2ccd433a070385c"),
+      "active" : true,
+      "label" : "Koniec planu produkcji w danym dniu na daną linię"
+    },
+
+    /* 7 createdAt:2020-08-04T20:54:18.000Z*/
+    {
+      "_id" : ObjectId("5f29cafab2ccd433a0703826"),
+      "active" : true,
+      "label" : "Brak detali z malarni na czas"
+    },
+
+    /* 8 createdAt:2020-08-04T20:53:11.000Z*/
+    {
+      "_id" : ObjectId("5f29cab7b2ccd433a070381f"),
+      "active" : true,
+      "label" : "Błąd operatora produkcji"
+    }
+  ], {ordered: false});
+}
+catch (err) {}
