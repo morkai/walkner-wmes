5f1dc6be4b2aa7bf3de183b8001849182460f55d
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-09-07 00:12:54 +0200

    planning: generator: apply the `production.taktTime.coeffs` setting to the PCE & MH times.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -6,3 +6,7 @@
 db.comprelentries.dropIndex({'orders._id': 1});
 db.comprelentries.createIndex({'orders.orderNo': 1});
 db.comprelentries.createIndex({mrps: 1, status: 1, 'orders.orderNo': 1});
+
+db.plansettings.updateMany({_id: {$gte: new Date('2020-09-09T00:00:00.000Z')}}, {$set: {
+  schedulingRate: {ANY: 1}
+}});
