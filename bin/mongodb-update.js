a2f05572906011e3fbd822552a6a8fdd3e1e9469
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-09-22 23:45:07 +0200

    mongodb-update: create the Machining section/family.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -5,3 +5,34 @@
 
 db.kaizenproductfamilies.updateMany({owners: {$exists: true}}, {$unset: {owners: 1}});
 db.kaizenproductfamilies.updateMany({coordSections: {$exists: false}}, {$set: {coordSections: []}});
+db.kaizenproductfamilies.insertOne({
+  "_id": "OM",
+  "position": 300,
+  "active": true,
+  "mrps": [],
+  "coordSections": [
+    {
+      "funcs": [
+        "process-engineer"
+      ],
+      "section": "OM",
+      "mor": "all"
+    }
+  ],
+  "name": "Obróbka mechaniczna (Tłocznia)",
+  "__v": 0
+});
+db.kaizensections.insertOne({
+  "_id": "OM",
+  "active": true,
+  "position": 4,
+  "subdivisions": [
+    new ObjectId("529f2654cd8eea9824000012"),
+    new ObjectId("529f267acd8eea9824000016"),
+    new ObjectId("529f268fcd8eea982400001a")
+  ],
+  "confirmers": [],
+  "coordinators": [],
+  "name": "Obróbka mechaniczna (Tłocznia)",
+  "__v": 0
+});
