76f231af6f3a0f2a8bbff52c58bf7c0eb356277a
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-11-29 19:35:04 +0100

    Updated the frontend build.

--- frontend-build/app/isa/WhmanCollection.js
+++ frontend-build/app/isa/WhmanCollection.js
@@ -1,1 +1,1 @@
-define(["app/users/UserCollection"],function(e){"use strict";return e.extend({rqlQuery:"select(firstName,lastName,personellId,card,cardUid)&limit(0)&privileges=ISA%3AWHMAN"})});
+define(["app/users/UserCollection"],function(e){"use strict";return e.extend({rqlQuery:"select(firstName,lastName,personnelId,card,cardUid)&limit(0)&privileges=ISA%3AWHMAN"})});
