b6b6268ae74886d487dea73caf7fdf3504f0b678
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-12-06 18:48:57 +0100

    Updated the frontend build.

--- frontend-build/app/wmes-compRel-entries/EntryCollection.js
+++ frontend-build/app/wmes-compRel-entries/EntryCollection.js
@@ -1,1 +1,1 @@
-define(["../core/Collection","./Entry"],function(e,t){"use strict";return e.extend({model:t,rqlQuery:"exclude(changes)&limit(-1337)&sort(-createdAt)"})});
+define(["../core/Collection","./Entry"],function(e,r){"use strict";return e.extend({model:r,rqlQuery:"exclude(orders,changes)&limit(-1337)&sort(-createdAt)"})});
