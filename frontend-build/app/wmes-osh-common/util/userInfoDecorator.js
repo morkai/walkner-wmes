605320b825a6384e3a585b398aa5af47832f98a2
Łukasz Walukiewicz <lukasz@miracle.systems>
2021-01-05 00:12:51 +0100

    Updated the frontend build.

--- frontend-build/app/wmes-osh-common/util/userInfoDecorator.js
+++ frontend-build/app/wmes-osh-common/util/userInfoDecorator.js
@@ -1,1 +1,1 @@
-define([],function(){"use strict";return function(e,o){o.oshWorkplace&&(e.oshWorkplace=o.oshWorkplace,e.oshDepartment=o.oshDepartment||0)}});
+define([],function(){"use strict";return function(o,e){e.oshWorkplace&&(o.oshDivision=e.oshDivision,o.oshWorkplace=e.oshWorkplace,o.oshDepartment=e.oshDepartment||0)}});
