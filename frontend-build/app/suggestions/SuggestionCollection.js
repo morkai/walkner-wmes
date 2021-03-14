420c621df4e88a0f7e6bc9405d6035fb82ef6e60
Łukasz Walukiewicz <lukasz@miracle.systems>
2021-03-14 20:18:46 +0100

    Updated the frontend build.

--- frontend-build/app/suggestions/SuggestionCollection.js
+++ frontend-build/app/suggestions/SuggestionCollection.js
@@ -1,1 +1,1 @@
-define(["../core/Collection","./Suggestion"],function(e,t){"use strict";return e.extend({model:t,theadHeight:2,rowHeight:1.5,rqlQuery:"exclude(changes)&limit(-1337)&sort(-date)"})});
+define(["../core/Collection","./Suggestion"],function(e,t){"use strict";return e.extend({model:t,theadHeight:2,rowHeight:1,rqlQuery:"exclude(changes)&limit(-1337)&sort(-date)"})});
