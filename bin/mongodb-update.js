e4b46b2197a2a71232cfdff78be9681eea73ca48
Łukasz Walukiewicz <lukasz@miracle.systems>
2021-03-29 13:14:42 +0200

    paintShop: show component item.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -3,8 +3,34 @@
 
 'use strict';
 
-db.kaizensections.updateMany({entryTypes: {$exists: false}}, {$set: {
-  entryTypes: ['kaizenOrders', 'suggestions', 'observations', 'minutes'],
-  auditors: [],
-  controlLists: []
-}});
+db.paintshoporders.find({}).forEach(psOrder =>
+{
+  psOrder.childOrders.forEach(childOrder =>
+  {
+    var sapOrder = db.orders.findOne({_id: childOrder.order}, {bom: 1});
+
+    if (!sapOrder)
+    {
+      childOrder.components.forEach(component =>
+      {
+        component.item = '';
+      });
+
+      return;
+    }
+
+    var codeToItem = {};
+
+    sapOrder.bom.forEach(component =>
+    {
+      codeToItem[component.nc12] = component.item;
+    });
+
+    childOrder.components.forEach(component =>
+    {
+      component.item = codeToItem[component.nc12] || '';
+    });
+  });
+
+  db.paintshoporders.updateOne({_id: psOrder._id}, {$set: {childOrders: psOrder.childOrders}});
+});
